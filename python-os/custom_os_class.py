4a36b4b134c8205e6a53203dd21fee09e1a89935
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-16 13:41:38 -0500

    custom_os_class

---
+++ python-os/custom_os_class.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+
+# Class definitions should use CamelCase convention based on pep-8 guidelines
+class CustomOs:
+
+
+    # Initialize the class with filepath and filename
+    def __init__(self, _my_file_path, _my_file_name):
+        self.my_filepath = _my_file_path
+        self.my_filename = _my_file_name
+        self.current_directory = os.getcwd()
