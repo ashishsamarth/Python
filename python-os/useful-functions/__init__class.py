38c2a40b109a0ffe5f9486a0bc3b6a0d015217ae
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-16 13:51:44 -0500

    __init__class

--- python-os/useful-functions/__init__class.py
+++ python-os/useful-functions/__init__class.py
@@ -1,4 +1,9 @@
 import os
 import sys
 
+
 # Class definitions should use CamelCase convention based on pep-8 guidelines
+class CustomOs:
+
+    # Initialize the class with filepath and filename
+    def __init__(self, _my_file_path, _my_file_name):
