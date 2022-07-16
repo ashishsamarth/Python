beb9d87927c79151eaf08c0626c233e4bc42675e
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-16 13:54:15 -0500

    go_back_n_dirs

--- python-os/useful-functions/go_back_n_dirs.py
+++ python-os/useful-functions/go_back_n_dirs.py
@@ -2,3 +2,6 @@ import os
 
 # Method to go back n number of directories
 # Argument to this method is: number of directories to go back
+
+def go_back_n_dirs(self, num_of_dirs: int):
+    # method variable to hold the message
