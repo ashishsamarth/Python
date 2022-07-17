910a2540993a787e935322ec7f6aea15d4338646
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:34:57 -0500

    cnt_occurrences_of_str_keys

---
+++ python-string/useful-functions/cnt_occurrences_of_str_keys.py
@@ -0,0 +1,7 @@
+import collections
+
+# Method to get unique alphabets from string
+@staticmethod
+# Argument to this method: string
+def cnt_occurrences_of_str_keys(_my_string_1):
+    return collections.Counter(_my_string_1).keys()
