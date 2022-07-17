5c4f2188bcc9ec137a66b5da840a0b8cc2d96bf4
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:41:17 -0500

    ret_str_0_9

---
+++ python-string/useful-functions/ret_str_0_9.py
@@ -0,0 +1,8 @@
+# Import String Package
+import string
+
+# Method to get numbers 0 through 9 as strings
+@staticmethod
+# Argument to this method: None
+def ret_str_0_9():
+    return string.digits
