699b03c891fa6ea8e4954ab6463320b1cdbf8ba8
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:38:33 -0500

    ret_alpha

---
+++ python-string/useful-functions/ret_alpha.py
@@ -0,0 +1,8 @@
+# Import String Package
+import string
+
+# Method to get both lower case and upper case letters (a through Z)
+@staticmethod
+# Argument to this method: None
+def ret_alpha():
+    return string.ascii_letters
