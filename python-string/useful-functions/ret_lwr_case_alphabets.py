d682a6cbe9860985932f8c97e8c5e7f0d62d64a5
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:39:41 -0500

    ret_lwr_case_alphabets

---
+++ python-string/useful-functions/ret_lwr_case_alphabets.py
@@ -0,0 +1,8 @@
+# Import String Package
+import string
+
+# Method to get only lower cases letters (a through z)
+@staticmethod
+# Argument to this method: None
+def ret_lwr_case_alphabets():
+    return string.ascii_lowercase
