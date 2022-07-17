d0f6f36939327071e5942acc76e1a6a558517c75
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-16 19:09:07 -0500

    custom_string_class

--- python-string/custom_string_class.py
+++ python-string/custom_string_class.py
@@ -71,5 +71,10 @@ class CustomString:
         if _my_string == reversed_string:
             return True
         return False
-    
-    
+
+
+    # Method to check if two strings are anagrams
+    @staticmethod
+    def strs_are_anagram(_my_string_1, _my_string_2):
+        # returns a dictionary where each alphabet of string is 'key' and their # of occurrences are values
+        return collections.Counter(_my_string_1) == collections.Counter(_my_string_2)
