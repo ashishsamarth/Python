2095a3b86f60f74d1ab9867148cc21ecec6b6f33
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:46:32 -0500

    ret_upr_case_alphabets

---
+++ python-string/useful-functions/ret_upr_case_alphabets.py
@@ -0,0 +1,8 @@
+# Import String Package
+import string
+
+# Method to get only upper case letters (A through Z)
+@staticmethod
+# Argument to this method: None
+def ret_upr_case_alphabets():
+    return string.ascii_uppercase
