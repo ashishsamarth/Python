9af9f4ca174ed22c2e015dfd3ecb1c17758d8c56
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 10:17:49 -0500

    convert_str_to_list

---
+++ python-string/useful-functions/convert_str_to_list.py
@@ -0,0 +1,11 @@
+# Method to convert a string to a list
+@staticmethod
+# Argument to this method: string
+def str_to_list(_my_string):
+    # declare a empty list
+    data_type_align = []
+    for _ in _my_string:
+        # Append elements of the string to list
+        data_type_align.append(_)
+    # Return type is a list
+    return data_type_align
