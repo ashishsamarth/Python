3460b66c116130bbb43061b9a4e0bc6bdc6b05e9
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-16 19:11:55 -0500

    custom_string_class

--- python-string/custom_string_class.py
+++ python-string/custom_string_class.py
@@ -85,3 +85,14 @@ class CustomString:
     def cnt_occurrences_of_str(_my_string_1):
         # returns a dictionary where each alphabet of string is 'key' and their # of occurrences are values
         return collections.Counter(_my_string_1)
+
+    # Method to get occurrences of alphabets in string
+    # Sort the result dictionary by key or value
+    # Order the dictionary in asc or desc
+    @staticmethod
+    def cnt_occurrences_of_str_ordered_rslt(_my_string_1, _sort_key, _sort_type):
+        result_set = collections.Counter(_my_string_1)
+        if _sort_key == 'k':
+            return collections.OrderedDict(sorted(result_set.items(), key=lambda _: _[0], reverse=_sort_type))
+        elif _sort_key == 'v':
+            return collections.OrderedDict(sorted(result_set.items(), key=lambda _: _[1], reverse=_sort_type))
