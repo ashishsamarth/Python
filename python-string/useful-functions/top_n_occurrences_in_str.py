6b8d458e1be9f4a6a74908944ca2ef2fa152c85e
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:50:48 -0500

    top_n_occurrences_in_str

---
+++ python-string/useful-functions/top_n_occurrences_in_str.py
@@ -0,0 +1,8 @@
+import collections
+
+# Method to get top 5 alphabets based on their occurrences in string
+@staticmethod
+# Arguments to this method: String and Integer
+def top_n_occurrences_in_str(_my_string_1, num_of_occurences):
+    # We use the 'most_common' method for Counter
+    return dict(collections.Counter(_my_string_1).most_common(num_of_occurences))
