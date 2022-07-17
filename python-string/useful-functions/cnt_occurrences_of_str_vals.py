21b917ec01942013f6123d31537ea954287620c8
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:37:02 -0500

    cnt_occurrences_of_str_vals

---
+++ python-string/useful-functions/cnt_occurrences_of_str_vals.py
@@ -0,0 +1,7 @@
+import collections
+
+# Method to get occurrences of alphabets in string
+@staticmethod
+# Argument to this method: string
+def cnt_occurrences_of_str_vals(_my_string_1):
+    return collections.Counter(_my_string_1).values()
