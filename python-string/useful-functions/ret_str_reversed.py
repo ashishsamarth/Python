cf50ac55d75125265799969992c62ed38f7dba48
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-17 09:42:58 -0500

    ret_str_reversed

---
+++ python-string/useful-functions/ret_str_reversed.py
@@ -0,0 +1,8 @@
+# Import String Package
+import string
+
+# Method to reverse the string
+@staticmethod
+# Argument to this method: string
+def ret_str_reversed(_my_string):
+    return _my_string[::-1]
