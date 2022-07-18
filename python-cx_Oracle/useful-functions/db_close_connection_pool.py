50fe36c5869f0f4c2be34dba86a8e9d6dd6c7473
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-18 09:35:16 -0500

    Deleted

--- python-cx_Oracle/useful-functions/db_close_connection_pool.py
+++
@@ -1,4 +0,0 @@
-# Method to close the connection pool
-# Argument to this method:- None
-def db_close_conn_pool(self):
-    self.pool.close()
--- python-cx_Oracle/useful-functions/db_commit.py
+++
@@ -1,5 +0,0 @@
-# Method to manually commit the sql
-# Argument to this method: None
-# Note: This is defined, since connection.autocommit is set to False by default, and I have kept it that way
-def db_commit(self):
-    self.db_auto_connect.commit()
