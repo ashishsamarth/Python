6ec340198bbda500bc51daf84221cd9226463a67
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 11:00:44 -0500

    accessed_collection_find_one

---
+++ python-pymongo/useful-functions/accessed_collection_find_one.py
@@ -0,0 +1,10 @@
+# Method to see one sample document from a given collection name
+# Argument to this method: Collection Name
+def accessed_collection_find_one(self, _collection_name):
+    note = 'Collection Name provided is not a present in Connected Database'
+    # Assert if the provided collection name exists in the connected DB
+    assert _collection_name in self.get_collection_names(), note
+    # In case of Assertion error, the collection cannot be accessed
+    _my_collection = self.connected_db[_collection_name]
+    # return type is dict
+    return _my_collection.find_one()
