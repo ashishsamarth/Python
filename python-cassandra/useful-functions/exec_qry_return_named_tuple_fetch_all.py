f4f0acbb4ece10a7809c3cd6030f3bb88f26282b
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 11:09:29 -0500

    exec_qry_return_named_tuple_fetch_all

---
+++ python-cassandra/useful-functions/exec_qry_return_named_tuple_fetch_all.py
@@ -0,0 +1,9 @@
+# Method to execute a cql query
+# Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+def exec_qry_return_named_tuple_fetch_all(self, _query):
+    session = self.db_session()
+    # This line sets up the return type of the query as a named tuple
+    session.row_factory = named_tuple_factory
+    row_result_as_named_tuple = session.execute(_query).all()
+    # return type of this method is a list of named tuples
+    return row_result_as_named_tuple     
