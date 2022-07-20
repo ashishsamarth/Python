1efd75373112acaa9457c8c063677937677c6247
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 11:13:26 -0500

    exec_qry_return_ordered_dict_fetch_all

---
+++ python-cassandra/useful-functions/exec_qry_return_ordered_dict_fetch_all.py
@@ -0,0 +1,9 @@
+# Method to execute a cql query
+# Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+def exec_qry_return_ordered_dict_fetch_all(self, _query):
+    session = self.db_session()
+    # This line sets up the return type of the query as an ordered dictionary
+    session.row_factory = ordered_dict_factory
+    row_result_as_ordered_dict = session.execute(_query).all()
+    # return type of this method is a list of ordered dictionaries
+    return row_result_as_ordered_dict
