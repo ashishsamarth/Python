a3e8968274a2f1feecd763d1b6e60490ba3ed897
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 11:05:39 -0500

    db_session

---
+++ python-cassandra/useful-functions/db_session.py
@@ -0,0 +1,8 @@
+# Method to connect to Cassandra Cluster on the remote host
+def db_session(self):
+    # Read the host, port and authorization params from the config file
+    # The configurations must match with ~.cassandra/cqlshrc on server
+    cluster = Cluster([self.host], port=self.port, auth_provider=self.auth)
+    # connect to the cluster and get the session
+    session = cluster.connect(self.my_keyspace)
+    return session
