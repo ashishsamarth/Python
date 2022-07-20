eab8d0dbf5a638e47564a377e2523b8064d5a552
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 10:54:27 -0500

    cassandra_conf

---
+++ python-cassandra/cassandra_conf.py
@@ -0,0 +1,5 @@
+cassandra_config = {'cassandra_bin': '/opt/cassandra/node1/bin',
+                    'cassandra_host': '192.168.1.106', 
+                    'cassandra_port': 9041,
+                    'cassandra_user': 'cassandra',
+                    'cassandra_password': 'cassandra'}
