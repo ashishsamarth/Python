be36feae8094f8117c4e6923d25d9adb55db7c39
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-20 11:04:30 -0500

    set_keyspace

---
+++ python-cassandra/useful-functions/set_keyspace.py
@@ -0,0 +1,6 @@
+# Method to set the user provided keyspace name
+def set_keyspace(self, _keyspace_name):
+    # Update the __init__ property to be used during session creation for keyspace
+    # This approach is also helpful for query execution without passing the keyspace everytime
+    self.my_keyspace = _keyspace_name
+    return self.my_keyspace
