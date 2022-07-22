5977495f702b2d6a88a557a6cf21e9165b11f96b
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-21 19:23:25 -0500

    exec_select_return_dict_fetch_one

---
+++ python-cassandra/useful-functions/exec_select_return_dict_fetch_one.py
@@ -0,0 +1,9 @@
+# Method to execute a cql query
+# Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+def exec_select_return_dict_fetch_one(self, _query):
+    session = self.db_session()
+    # This line sets up the return type of the query as a dictionary
+    session.row_factory = dict_factory
+    row_result_as_dict = session.execute(_query).one()
+    # return type of this method is a dictionary
+    return row_result_as_dict
