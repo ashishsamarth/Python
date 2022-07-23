cbf41e05d1201f6b726b0cffa3ba790cbf195ac5
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-22 23:26:06 -0500

    manage_cassandra_class

---
+++ python-cassandra/manage_cassandra_class.py
@@ -0,0 +1,8 @@
+# Import paramiko module : This is the module to handle remote connections
+import paramiko
+# Import paramiko_conf : This holds the connection params
+import paramiko_conf
+
+
+# Class definitions should use CamelCase convention based on pep-8 guidelines
+class ManageCassandra:
