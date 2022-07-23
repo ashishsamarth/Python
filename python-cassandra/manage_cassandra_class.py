c94e80a7dd058d8c311af8047ef2ad82c102350d
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-22 23:32:07 -0500

    manage_cassandra_class

--- python-cassandra/manage_cassandra_class.py
+++ python-cassandra/manage_cassandra_class.py
@@ -30,3 +30,7 @@ class ManageCassandra:
     # Method to execute specific commands on the connected host
     # Argument to this method is: command provided by end user
     def execute_command(self, _command):
+        stdin, stdout, stderr = self.ssh.exec_command(_command)
+        # standard output from the command execution is sent to a variable
+        command_output = stdout.read()
+        
