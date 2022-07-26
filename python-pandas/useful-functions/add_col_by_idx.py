196fbf025c7d31a8cdb23d0a49cd7d245b7cb860
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-25 21:50:13 -0500

    add_col_by_idx

---
+++ python-pandas/useful-functions/add_col_by_idx.py
@@ -0,0 +1,9 @@
+# Method to add a new column using the index position in the existing dataframe
+def add_col_by_idx(self, position, _col_name):
+    # The following statement checks if the user provided column name already exists in the dataframe or not
+    # The new column will only be added if it does not exist already
+    if _col_name not in self.my_df.columns:
+        return self.my_df.insert(position, _col_name, '', True)
+    # Else the new column name will not be added to the dataframe
+    else:
+        print(f'Column :- "{_col_name}" is already present in the Dataframe, skipping addition...', end='\n')
