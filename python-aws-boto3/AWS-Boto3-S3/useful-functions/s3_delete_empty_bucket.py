c062dd6a550c3aff4813b25938a3a843e2d0377d
ashishsamarth <mrashishsamarth@gmail.com>
2022-07-25 21:42:51 -0500

    s3_delete_empty_bucket

---
+++ python-aws-boto3/AWS-Boto3-S3/useful-functions/s3_delete_empty_bucket.py
@@ -0,0 +1,31 @@
+import boto3
+from botocore.exceptions import ClientError
+
+class AwsBoto3S3:
+
+    AWS_REGION = {'loc_1': 'us-east-1'}
+
+    S3_Error_Map = {'InvalidBucketName': 'BucketName does not align with S3 naming convention',
+                    'BucketAlreadyExists': 'The requested bucket name is not available.',
+                    'IllegalLocationConstraintException': 'The unspecified location constraint is incompatible for the region',
+                    'NoSuchBucket': 'The specified bucket does not exist'}
+
+# Method to delete empty bucket
+# Argument to this method is:- Bucket Name
+def s3_delete_empty_bucket(self, _bucket_name):
+    try:
+        # Pull the objects in the specified bucket in to a variable (return type is a dictionary)
+        item = self.s3_client.list_objects(Bucket=_bucket_name)
+        # Special Note:- If a bucket contains any kind of files / folders
+        # Then 'Contents' will be one of the keys in the item (dictionary)
+        # If the bucket is empty, then Key='Contents' will not exist in the response (item) dictionary
+        # Following line validates, if the bucket is empty
+        if 'Contents' not in item:
+            # proceed with calling the 'delete_objects' method on the S3 object
+            self.s3_client.delete_bucket(Bucket=_bucket_name)
+            # Print the following method on the terminal
+            print(str(_bucket_name) + ':- ' + 'was deleted successfully')
+    # In case code encounters common error, a brief message of the error will be presented to the user
+    except ClientError as error_data:
+        if error_data.response['Error']['Code'] in AwsBoto3S3.S3_Error_Map.keys():
+            print(str(_bucket_name) + ':- ' + AwsBoto3S3.S3_Error_Map[error_data.response['Error']['Code']])
