085f28eaa4514c427203e3233efef8a84eaa7d2a
ashish samarth <mrashishsamarth@gmail.com>
2022-07-31 11:00:52 -0500

    get_architecture

---
+++ useful-functions/get_architecture.py
@@ -0,0 +1,8 @@
+# Import the plaform module
+import platform
+# Method to get the architectural informartion of the machine
+def get_architecture():
+    # The 'machine' method provides the architectural information of the local machine
+    return platform.machine()
+    # Another way to get the same information is via uname's named tuple result
+    # return platform.uname().machine
