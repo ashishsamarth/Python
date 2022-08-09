bed91853e479a324ac1f12e4252c3565e7aba813
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-09 15:37:30 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -10,3 +10,12 @@ class UsefulUtils:
     def get_arg_count(*_arg):
         # Return type is an integer
         return len(_arg)
+    
+    # Method to assign same values to provided keys
+    # Arguments to this method: _keys as a list, _value as any value
+    def assign_same_val_to_keys(_keys, _value):
+        # itertools.repeat : repeats the passed value seamlessly
+        # zip : ties the keys and value together together
+        my_dict = dict(zip(_keys, itertools.repeat(_value)))
+        # Return type is a dictionary
+        return my_dict
