ecc3ca245465e52247f969b0a1b64fed3f764b50
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-10 07:48:54 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -113,6 +113,7 @@ class UsefulUtils:
     # Argument to this method: Multiple Dictionaries
     # * takes care of the unpacking multiple dictionaries passed as arguments
     def join_dicts_sort_by_keys_desc(*dicts):
+        # Create an empty dictionary
         merged_dict = {}
         for _ in dicts:
             merged_dict |= _
