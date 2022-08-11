9b27c9c3b07d1f75e0e12c6b95db3a79071660ba
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 14:11:26 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -26,8 +26,11 @@ class UsefulUtils:
 
     # Method to create a dictionary dataset automatically
     # Argument to this method: None
+    # Keys are - Upper case Alpbabets
+    # Values are - Their count of occurrence
     def auto_create_dict_by_counter():
         my_str = UsefulUtils.get_upper_case_alphabets()
+        # Return type is a dictionary
         return dict(collections.Counter(my_str))
 
     # Method to check if given string is empty or not
