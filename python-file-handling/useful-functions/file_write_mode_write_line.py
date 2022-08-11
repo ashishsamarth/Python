4bdca981ed1069dd85c186fb8b8969233b9e5da4
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 10:57:22 -0500

    file_write_mode_write_line

--- python-file-handling/useful-functions/file_write_mode_write_line.py
+++ python-file-handling/useful-functions/file_write_mode_write_line.py
@@ -2,7 +2,7 @@
 def file_write_mode_write_line(self, _text, in_newline):
     with open(self.my_filename, 'w') as write_mode:
         # Based on the value provided for argument in_newline the text will be written with or without newline
-        if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+        if in_newline.lower() in ['y', 'yes']:
             write_mode.writelines(_text + '\n')
-        elif in_newline in ['n','N','NO', 'No', 'no']:
+        elif in_newline.lower() in ['n', 'no']:
             write_mode.writelines(_text)
