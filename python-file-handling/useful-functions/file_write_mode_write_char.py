ff0a9d4ba8b9affb6abcc6edeae846cd90e1cdb3
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 10:57:11 -0500

    file_write_mode_write_char

--- python-file-handling/useful-functions/file_write_mode_write_char.py
+++ python-file-handling/useful-functions/file_write_mode_write_char.py
@@ -3,7 +3,7 @@ def file_write_mode_write_char(self, data_set, in_newline):
     # open the filename captured during initialization in write mode
     with open(self.my_filename, 'w') as write_mode:
         # Based on the value provided for argument in_newline the text will be written with or without newline
-        if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+        if in_newline.lower() in ['y', 'yes']:
             write_mode.write(data_set + '\n')
-        elif in_newline in ['n','N','NO', 'No', 'no']:
+        elif in_newline.lower() in ['n', 'no']:
             write_mode.write(data_set)
