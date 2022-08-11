2aa891abcf4e6f4372f243232a4c0ce84e8abfb9
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 13:17:20 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -338,9 +338,27 @@ class UsefulUtils:
         # Following will pick the first value from the dictionary with max count of occurence
         # If their are others, they will be ignored
         max_first_value = max(occurence_dict, key=occurence_dict.get)
-        # Return type is a List
+        # Return type dependent on key
         return max_first_value
-            
+
+    # Method to return last value by max count of occurence
+    # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
+    # * takes care of the unpacking multiple dictionaries passed as arguments
+    def get_last_val_by_max_occurence_in_joined_dicts(*_dicts_iterables):
+        # Create an empty dictionary
+        merged_dicts = {}
+        # Iterate over all the dictionaries in the unpacked list of dictionaries
+        for _ in _dicts_iterables:
+            # Update the Blank dictionary with keys and Values from unpacked list of dictionaries
+            merged_dicts |= _
+        # Count the Occurence of each value in merged dictionary and keep value as key and count as value
+        occurence_dict = dict(collections.Counter(merged_dicts.values()))
+        # Following will pick the last value from the dictionary with max count of occurence
+        # If their are others, they will be ignored
+        max_last_value = max(reversed(occurence_dict), key=occurence_dict.get)
+        # Return type dependent on key
+        return max_last_value
+
     # Method to return Values by min count of occurence
     # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
     # * takes care of the unpacking multiple dictionaries passed as arguments
