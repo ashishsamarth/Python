2ca2f4059144b56bc33a03caa8cc6a27824e4707
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 10:57:35 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -56,18 +56,18 @@ class CustomFileHandler:
         # open the filename captured during initialization in write mode
         with open(self.my_filename, 'w') as write_mode:
             # Based on the value provided for argument in_newline the text will be written with or without newline
-            if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+            if in_newline.lower() in ['y', 'yes']:
                 write_mode.write(data_set + '\n')
-            elif in_newline in ['n','N','NO', 'No', 'no']:
+            elif in_newline.lower() in ['n', 'no']:
                 write_mode.write(data_set)
 
      # Method to write user provided text to a file in single line followed by a new line character
     def file_write_mode_write_line(self, _text, in_newline):
         with open(self.my_filename, 'w') as write_mode:
             # Based on the value provided for argument in_newline the text will be written with or without newline
-            if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+            if in_newline.lower() in ['y', 'yes']:
                 write_mode.writelines(_text + '\n')
-            elif in_newline in ['n','N','NO', 'No', 'no']:
+            elif in_newline.lower() in ['n', 'no']:
                 write_mode.writelines(_text)
 
     # Method to truncate the file using the append mode
@@ -80,9 +80,9 @@ class CustomFileHandler:
         # open the filename captured during initialization in append mode
         with open(self.my_filename, 'a') as append_mode:
             # Based on the value provided for argument in_newline the text will be written with or without newline
-            if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+            if in_newline.lower() in ['y', 'yes']:
                 append_mode.write(data_set + '\n')
-            elif in_newline in ['n','N','NO', 'No', 'no']:
+            elif in_newline.lower() in ['n', 'no']:
                 append_mode.write(data_set)
 
     # Method to separate the text printed on the terminal
