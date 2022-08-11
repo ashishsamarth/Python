6bcfc444adc859fc0b886108832213d3e5150160
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-11 10:56:58 -0500

    file_append_mode

--- python-file-handling/useful-functions/file_append_mode.py
+++ python-file-handling/useful-functions/file_append_mode.py
@@ -3,7 +3,7 @@ def file_append_mode(self, data_set, in_newline):
     # open the filename captured during initialization in append mode
     with open(self.my_filename, 'a') as append_mode:
         # Based on the value provided for argument in_newline the text will be written with or without newline
-        if in_newline in ['y', 'Y','Yes', 'YES', 'yes']:
+        if in_newline.lower() in ['y', 'yes']:
             append_mode.write(data_set + '\n')
-        elif in_newline in ['n','N','NO', 'No', 'no']:
+        elif in_newline.lower() in ['n', 'no']:
             append_mode.write(data_set)
