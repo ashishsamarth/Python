421542be02db56bc00c598c17c1d9aea0bb25c9b
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-12 18:00:48 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -99,14 +99,13 @@ class UsefulUtils:
     def concatenate_elems_of_lists(*_inp_list, case='original'):
         # Following is a dictionary with case values as keys and corresponding list comprehensions as their evaluated values
         # itertools.chain(*_inp_list) will join all the lists in to one list
-        # Each element of the list will be evaluated and only elements having alphabet will be passed over
-        # Each passed over element is then joined using space as delimiter and then strin function based on user selection are applied
-        conditional = {'capitalize' : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()).capitalize(),
-                       'lower'      : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()).lower(),
-                       'original'   : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()),
-                       'swapcase'   : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()).swapcase(),
-                       'title'      : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()).title(),
-                       'upper'      : ' '.join(_ for _ in itertools.chain(*_inp_list) if str(_).isalpha()).upper()}
+        # Each element of the list will be joined using space as delimiter and then string function based on user selection are applied
+        conditional = {'capitalize' : ' '.join(_ for _ in itertools.chain(*_inp_list)).capitalize(),
+                       'lower'      : ' '.join(_ for _ in itertools.chain(*_inp_list)).lower(),
+                       'original'   : ' '.join(_ for _ in itertools.chain(*_inp_list)),
+                       'swapcase'   : ' '.join(_ for _ in itertools.chain(*_inp_list)).swapcase(),
+                       'title'      : ' '.join(_ for _ in itertools.chain(*_inp_list)).title(),
+                       'upper'      : ' '.join(_ for _ in itertools.chain(*_inp_list)).upper()}
         # Return type is a string
         return conditional.get(case)
 
