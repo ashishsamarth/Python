1c44e605b9f67ea97547beaea3785fd14d91f4b6
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-13 09:55:45 -0500

    get_alphabets

---
+++ python-utilities/useful-functions/get_alphabets.py
@@ -0,0 +1,5 @@
+# Method to get both lower case and upper case letters (a through Z)
+# Argument to this method: None
+def get_alphabets():
+    # Return type is a string
+    return string.ascii_letters
