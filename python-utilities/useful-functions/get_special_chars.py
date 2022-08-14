fd652be4ac005fa12e804735c0d2b008913cecf4
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-14 10:56:45 -0500

    get_special_chars

---
+++ python-utilities/useful-functions/get_special_chars.py
@@ -0,0 +1,6 @@
+# Method to get special characters
+# Argument to this method: None
+def get_special_chars():
+    # Return type is a string
+    # returns: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    return string.punctuation
