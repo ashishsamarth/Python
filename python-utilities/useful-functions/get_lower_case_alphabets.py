779fc71a853ae4edfe15f126f51dc21ca9bd0098
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-14 10:54:55 -0500

    get_lower_case_alphabets

---
+++ python-utilities/useful-functions/get_lower_case_alphabets.py
@@ -0,0 +1,5 @@
+# Method to get only lower case letters (a through z)
+# Argument to this method: None
+def get_lower_case_alphabets():
+    # Return type is a string
+    return string.ascii_lowercase
