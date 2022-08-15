1184689e0044cd9f57e0412414f4852bd7dfe6c6
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 07:52:22 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -372,7 +372,7 @@ class UsefulUtils:
     def get_str_0_7():
         # Return type is a string
         return string.octdigits
-        
+
     # Method to get numbers 0 through 9 as string
     # Argument to this method: None
     def get_str_0_9():
@@ -405,24 +405,6 @@ class UsefulUtils:
         # Return type is a string
         return string.ascii_uppercase
 
-    # Method to return Values by min count of occurrence
-    # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
-    # * takes care of the unpacking multiple dictionaries passed as arguments
-    def get_vals_by_min_occurrence_in_joined_dicts(*_dicts_iterables):
-        # Create an empty dictionary
-        merged_dicts = {}
-        # Iterate over all the dictionaries in the unpacked list of dictionaries
-        for _ in _dicts_iterables:
-            # Update the empty dictionary with keys and Values from unpacked list of dictionaries
-            merged_dicts |= _
-        # Count the Occurrence of each value in merged dictionary and keep value as key and occurrence count as value            
-        occurrence_dict = dict(collections.Counter(merged_dicts.values()))
-        # List comprehension to get the list of values with min occurrence in dictionaries
-        # Using list comprehension, since their may be mutliple values having same occurence count as min count
-        min_value = [k for k, v in occurrence_dict.items() if v == min(occurrence_dict.values())]
-        # Return type is a List
-        return min_value
-
     # Method to return Values by max count of occurrence
     # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
     # * takes care of the unpacking multiple dictionaries passed as arguments
@@ -440,6 +422,24 @@ class UsefulUtils:
         max_value = [k for k, v in occurrence_dict.items() if v == max(occurrence_dict.values())]
         # Return type is a List
         return max_value
+        
+    # Method to return Values by min count of occurrence
+    # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
+    # * takes care of the unpacking multiple dictionaries passed as arguments
+    def get_vals_by_min_occurrence_in_joined_dicts(*_dicts_iterables):
+        # Create an empty dictionary
+        merged_dicts = {}
+        # Iterate over all the dictionaries in the unpacked list of dictionaries
+        for _ in _dicts_iterables:
+            # Update the empty dictionary with keys and Values from unpacked list of dictionaries
+            merged_dicts |= _
+        # Count the Occurrence of each value in merged dictionary and keep value as key and occurrence count as value            
+        occurrence_dict = dict(collections.Counter(merged_dicts.values()))
+        # List comprehension to get the list of values with min occurrence in dictionaries
+        # Using list comprehension, since their may be mutliple values having same occurence count as min count
+        min_value = [k for k, v in occurrence_dict.items() if v == min(occurrence_dict.values())]
+        # Return type is a List
+        return min_value
 
     # Method to check if a string is a palindrome
     # Argument to this method: string
