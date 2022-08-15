1a86c2bd3c80eefb9030a4995ec0c50adda14092
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 07:47:32 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -250,7 +250,7 @@ class UsefulUtils:
     def get_arg_cnt(*_arg):
         # Return type is an integer
         return len(_arg)
-        
+
     # Method to extract digits from given list of strings
     # Argument to this method: Multiple strings as arguments (separated by comma), user provided delimiter
     # * takes care of the unpacking multiple strings passed as arguments
@@ -261,19 +261,7 @@ class UsefulUtils:
         # The Outer for loop simply iterates over all the elements of the concatenated string and filter only digits
         # These numerical values are then joined with user provided or default delimiter
         # Return type is string
-        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if _.isdigit())
-
-    # Method to extract special characters from given list of strings
-    # Argument to this method: Multiple strings as arguments (separated by comma), user provided delimiter
-    # * takes care of the unpacking multiple strings passed as arguments
-    # Default value for delimiter is ''
-    def get_punctuations_from_strs(*_inp_strs, delim=''):
-        # Internal for loop is calling the 'concatenate_multiple_strs' method in the same class
-        # Result of this method is a concatenated string
-        # The Outer for loop simply iterates over all the elements of the concatenated string and filter only digits
-        # These numerical values are then joined with user provided or default delimiter
-        # Return type is string
-        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if not _.isalnum())         
+        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if _.isdigit())     
 
     # Method to return first value by max count of occurrence
     # Arguments to this method: Multiple dictionaries as arguments (separated by comma)
@@ -353,6 +341,18 @@ class UsefulUtils:
         # Return type is a string
         return string.ascii_lowercase
 
+    # Method to extract special characters from given list of strings
+    # Argument to this method: Multiple strings as arguments (separated by comma), user provided delimiter
+    # * takes care of the unpacking multiple strings passed as arguments
+    # Default value for delimiter is ''
+    def get_punctuations_from_strs(*_inp_strs, delim=''):
+        # Internal for loop is calling the 'concatenate_multiple_strs' method in the same class
+        # Result of this method is a concatenated string
+        # The Outer for loop simply iterates over all the elements of the concatenated string and filter only digits
+        # These numerical values are then joined with user provided or default delimiter
+        # Return type is string
+        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if not _.isalnum())  
+        
     # Method to get lower case a through f
     # Use of list comprehension with join
     # Argument to this method: None    
