7f784f4177735b8f0e6fd3cb9f687f170dbe140d
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 15:16:07 -0500

    join_tuples

--- python-utilities/useful-functions/join_tuples.py
+++ python-utilities/useful-functions/join_tuples.py
@@ -3,6 +3,8 @@
 # * takes care of the unpacking multiple tuples passed as arguments
 def join_tuples(*_tuple_iterables):
     # itertools.chain : joins multiple tuples in to one
-    joined_tuple = tuple(itertools.chain(*_tuple_iterables))
+    # _tuple_iterables.__class__() : Is a method to enforce that the output result is of same data-type as the input
+    # since __class__ will provide the datatype, same as type()
+    joined_tuple = _tuple_iterables.__class__(itertools.chain(*_tuple_iterables))
     # Return type is a tuple
     return joined_tuple
