7d060ed1c7609286ad3febd9a84e026fac64ecb8
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 10:39:28 -0500

    get_length_basic_dtypes

---
+++ python-utilities/useful-functions/get_length_basic_dtypes.py
@@ -0,0 +1,7 @@
+# Method to get length of input:
+# This method is limited to basic data types (int, str, list, tuple, dict)
+# Argument to this method: Input Variable
+# Utilizes the length_hint function from the operator module
+def get_length_basic_dtypes(_inp):
+    # Return type is an Integer
+    return operator.length_hint(str(_inp)) if type(_inp) is int else operator.length_hint(_inp)
