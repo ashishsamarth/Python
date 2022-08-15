84999e25cfbc8926bbc699a3a607672efbec43fa
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 07:49:07 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -281,24 +281,6 @@ class UsefulUtils:
         # Return type dependent on key
         return max_first_value
 
-    # Method to get Indices of a given element in provided strings
-    # Arguments to this method: Multiple Strings and Element for which we need indices, return type selected by user
-    # * takes care of the unpacking multiple dictionaries passed as arguments
-    # Default value for return_type is <list>
-    def get_indices_of_elem_in_str(*_inp_str, _elem, return_type='list'):
-        # Call the method concatenate_multiple_strs to join the provided strings
-        concatenated_str = UsefulUtils.concatenate_multiple_strs(*_inp_str)
-        # Following dictionary creates a case based logic
-        # Data type of values is changed based on the key selection by user
-        # Iterate over the elements concatenated string
-        # Using Enumerate and List Comprehension with Conditional statement, get the <list> of Indices of matched values
-        # Return type is dependent on user selection
-        # _inp_str.__class__ : Enforces the datatype of list comprehension to be same as Input <tuple>
-        # *_inp_str : Is a list of tuples when unpacked
-        conditional = { 'list' : [key for key, val in enumerate(concatenated_str) if val == _elem],
-                        'tuple' : _inp_str.__class__(key for key, val in enumerate(concatenated_str) if val == _elem)}
-        return conditional.get(return_type)
-
     # Method to get Indices of a given element in provided lists
     # Arguments to this method: Multiple lists and Element for which we need indices, return type selected by user
     # * takes care of the unpacking multiple dictionaries passed as arguments
@@ -315,6 +297,24 @@ class UsefulUtils:
         # *_inp_iterable : Is a list of tuples when unpacked
         conditional = { 'list' : [key for key, val in enumerate(chained_iterable) if val == _elem],
                         'tuple' : _inp_iterable.__class__(key for key, val in enumerate(chained_iterable) if val == _elem)}
+        return conditional.get(return_type)
+
+    # Method to get Indices of a given element in provided strings
+    # Arguments to this method: Multiple Strings and Element for which we need indices, return type selected by user
+    # * takes care of the unpacking multiple dictionaries passed as arguments
+    # Default value for return_type is <list>
+    def get_indices_of_elem_in_str(*_inp_str, _elem, return_type='list'):
+        # Call the method concatenate_multiple_strs to join the provided strings
+        concatenated_str = UsefulUtils.concatenate_multiple_strs(*_inp_str)
+        # Following dictionary creates a case based logic
+        # Data type of values is changed based on the key selection by user
+        # Iterate over the elements concatenated string
+        # Using Enumerate and List Comprehension with Conditional statement, get the <list> of Indices of matched values
+        # Return type is dependent on user selection
+        # _inp_str.__class__ : Enforces the datatype of list comprehension to be same as Input <tuple>
+        # *_inp_str : Is a list of tuples when unpacked
+        conditional = { 'list' : [key for key, val in enumerate(concatenated_str) if val == _elem],
+                        'tuple' : _inp_str.__class__(key for key, val in enumerate(concatenated_str) if val == _elem)}
         return conditional.get(return_type)
 
     # Method to return last value by max count of occurrence
@@ -352,7 +352,7 @@ class UsefulUtils:
         # These numerical values are then joined with user provided or default delimiter
         # Return type is string
         return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if not _.isalnum())  
-        
+
     # Method to get lower case a through f
     # Use of list comprehension with join
     # Argument to this method: None    
