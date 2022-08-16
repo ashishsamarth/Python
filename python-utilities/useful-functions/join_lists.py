5e223fa4fe08b36675c32c5a8421199b401df109
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-15 19:27:41 -0500

    join_lists

---
+++ python-utilities/useful-functions/join_lists.py
@@ -0,0 +1,8 @@
+# Method to join multiple lists into one list
+# Argument to this method: Multiple lists as arguments (separated by comma)
+# * takes care of the unpacking multiple lists passed as arguments
+def join_lists(*_lists_iterables):
+    # itertools.chain : joins the multiple lists into one
+    chained_list = list(itertools.chain(*_lists_iterables))
+    # Return type is a list
+    return chained_list
