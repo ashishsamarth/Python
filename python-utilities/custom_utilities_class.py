7e4a969d3167c99076ade22d2853d34001b2d997
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:57:28 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -101,7 +101,7 @@ class UsefulUtils:
     # _sort_key='key', _sort_reversal=False is defaulted to sorting by key in ascending order
     # _sort_key='value' will sort the resulting dictionary by values in ascending order
     # _sort_key='value', _sort_reversal=True will sort the resulting dictionary by values in descending order
-    def cnt_and_sort_occurrences_of_str_vals_in_concatenated_str(*_iterables, _sort_key='key', _sort_reversal=False):
+    def cnt_and_sort_occurrences_of_str_vals_in_concatenated_str(*_iterables, _sort_key='key', _sort_reversal=False) -> dict:
         # Use list comprehension to join the string without a space / delimiter
         joined_string = ''.join(_ for _ in _iterables)
         # Count the Occurrence of each element in joined string and keep element as key and occurrence count as value
