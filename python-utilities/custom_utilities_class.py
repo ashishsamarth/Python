ffcc644252e8b434b46f9d000ac9335c0ff212bc
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:29:36 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -593,10 +593,12 @@ class UsefulUtils:
         # Return type is a dictionary
         return dict(zip(key_list, val_list))
 
-    # Method to join multiple tuples arguments into one tuple and remove duplicate elements from result tuple
-    # Arguments to this method: Multiple tuples as arguments (separated by comma)
-    # * takes care of the unpacking multiple tuples passed as arguments
     def merge_tuples(*_tuple_iterables):
+        '''
+        Method to join multiple tuples arguments into one tuple and remove duplicate elements from result tuple
+        Arguments to this method: Multiple tuples as arguments (separated by comma)
+        * takes care of the unpacking multiple tuples passed as arguments
+        '''
         # itertools.chain : joins multiple tuples in to one
         # set : removes the duplicate elements from the result
         joined_tuple = tuple(set(itertools.chain(*_tuple_iterables)))
