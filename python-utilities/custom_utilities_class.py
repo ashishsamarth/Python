9c2767917c59a090693d1cef61622e9839ad8d78
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:26:57 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -645,9 +645,11 @@ class UsefulUtils:
         # Return type is dependent on the case provided by user
         return driver.get(case)
 
-    # Method to reverse the string
-    # Argument to this method: string
     def reverse_string(_inp_str):
+        '''
+        Method to reverse the string
+        Argument to this method: string
+        '''
         # Return type is a string
         return _inp_str[::-1]
 
