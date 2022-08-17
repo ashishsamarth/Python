8034d33527ad1a823af6ea35be3a72f5ff032986
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:26:20 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -651,10 +651,12 @@ class UsefulUtils:
         # Return type is a string
         return _inp_str[::-1]
 
-    # Method to join multiple lists into one list and sum all the digits
-    # Argument to this method: Multiple lists as arguments (separated by comma)
-    # * takes care of the unpacking multiple lists passed as arguments
     def sum_elems_of_lists(*_inp_list):
+        '''
+        Method to join multiple lists into one list and sum all the digits
+        Argument to this method: Multiple lists as arguments (separated by comma)
+        * takes care of the unpacking multiple lists passed as arguments
+        '''
         # itertools.chain(*_inp_list) : Joins all the lists into one list
         # Following List comprehension, filters only digits from the input list
         # Return type is an integer
