fd3ef0f95ecf9df15c4cf8d16f9be716590d6e5c
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:14:16 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -91,15 +91,19 @@ class UsefulUtils:
         # Return type is a dictionary
         return dict(collections.Counter(my_str))
 
-    # Method to check if given string is empty or not
-    # Argument to this method: string
     def chk_if_str_empty_m1(_inp_str) -> bool:
+        '''
+        Method to check if given string is empty or not
+        Argument to this method: string
+        '''
         # Return type is boolean
         return True if not _inp_str else False
 
-    # Method to check if given string is empty or not
-    # Argument to this method: string
     def chk_if_str_empty_m2(_inp_str) -> bool:
+        '''
+        Method to check if given string is empty or not
+        Argument to this method: string
+        '''
         # Return type is boolean
         # Utilizes Ternary Operator Approach
         # In Ternary Operators: False Boolean is written first and True Boolean is written next
