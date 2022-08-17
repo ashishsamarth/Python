eaec6eb70f12109161fea869911fa3941cfb447b
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:55:55 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -76,7 +76,7 @@ class UsefulUtils:
     # Argument to this method: None
     # Keys are - Upper case Alpbabets
     # Values are - Their count of occurrence
-    def auto_create_dict_by_counter():
+    def auto_create_dict_by_counter() -> dict:
         my_str = UsefulUtils.get_upper_case_alphabets()
         # Return type is a dictionary
         return dict(collections.Counter(my_str))
