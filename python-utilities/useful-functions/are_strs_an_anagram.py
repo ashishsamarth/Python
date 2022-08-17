90bf4d3f6439123babe3dd63e279b17d3392ef7d
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:52:09 -0500

    deleted

--- python-utilities/useful-functions/are_strs_an_anagram.py
+++
@@ -1,5 +0,0 @@
-# Method to check if two strings are anagrams
-# Argument to this method: two strings
-def are_strs_an_anagram(_inp_str_1, _inp_str_2):
-    # Return type is boolean
-    return True if collections.Counter(_inp_str_1) == collections.Counter(_inp_str_2) else False
