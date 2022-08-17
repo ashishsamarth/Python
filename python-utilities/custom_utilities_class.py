450f1eeccb5289f52c563d53e7855c32437806a3
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:55:32 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -65,7 +65,7 @@ class UsefulUtils:
 
     # Method to assign same values to provided keys and create a dictionary
     # Arguments to this method: _keys as multiple list of elements, _value as any value
-    def assign_same_val_to_all_keys(*_keys, _value):
+    def assign_same_val_to_all_keys(*_keys : list, _value) -> dict:
         # itertools.repeat : repeats the passed value seamlessly
         # zip : ties the keys and value together together
         # itertools.chain(*_keys) : Joins all lists to form a single list
