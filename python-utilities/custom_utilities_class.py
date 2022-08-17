f08207d32a4894559a4e81b6be84f732d6bfd28c
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:56:27 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -83,7 +83,7 @@ class UsefulUtils:
 
     # Method to check if given string is empty or not
     # Argument to this method: string
-    def chk_if_str_empty_m1(_inp_str):
+    def chk_if_str_empty_m1(_inp_str) -> bool:
         # Return type is boolean
         return True if not _inp_str else False
 
