76fd05525b58127276be3158c00c3e6ea0c5ca6e
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:54:25 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -57,7 +57,7 @@ class UsefulUtils:
     
     # Method to check if two strings are anagrams
     # Argument to this method: two strings
-    def are_strs_an_anagram_m2(_inp_str_1, _inp_str_2):
+    def are_strs_an_anagram_m2(_inp_str_1: str, _inp_str_2: str) -> bool:
         # Return type is boolean
         # Utilizes Ternary Operator Approach
         # In Ternary Operators: False Boolean is written first and True Boolean is written next
