4dffdc19874df3d6683ed2b72ba00402d598a839
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-16 21:56:53 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -89,7 +89,7 @@ class UsefulUtils:
 
     # Method to check if given string is empty or not
     # Argument to this method: string
-    def chk_if_str_empty_m2(_inp_str):
+    def chk_if_str_empty_m2(_inp_str) -> bool:
         # Return type is boolean
         # Utilizes Ternary Operator Approach
         # In Ternary Operators: False Boolean is written first and True Boolean is written next
