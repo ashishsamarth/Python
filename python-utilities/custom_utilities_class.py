d3c5deea1208deb84d8d89f68630bf9a39bdbacb
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:30:17 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -586,10 +586,12 @@ class UsefulUtils:
         # Return type is a tuple
         return joined_tuple
 
-    # Method to create a dictionary based on two lists
-    # Argument to this method: Two lists
-    # Elements of 1st list are treated as 'Keys' and 2nd list as 'Values'
     def lists_to_dict(key_list, val_list):
+        '''
+        Method to create a dictionary based on two lists
+        Argument to this method: Two lists
+        Elements of 1st list are treated as 'Keys' and 2nd list as 'Values'        
+        '''
         # Return type is a dictionary
         return dict(zip(key_list, val_list))
 
