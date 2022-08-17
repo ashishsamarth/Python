60fc8a0d65c9e7b24e025a0bc23b8fa20aadba52
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:16:36 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -149,9 +149,11 @@ class UsefulUtils:
         elif _sort_key == 'value':
             return dict(collections.OrderedDict(sorted(result_set.items(), key=lambda _: _[1], reverse=_sort_reversal)))
 
-    # Method to get occurrences of alphabets in string along with string
-    # Argument to this method: string
     def cnt_elem_occurrences_in_str(_inp_str : str) -> dict:
+        '''
+        Method to get occurrences of alphabets in string along with string
+        Argument to this method: string
+        '''
         # Return type is a dictionary where each alphabet of string is 'key' and their # of occurrences are values
         return dict(collections.Counter(_inp_str))
 
