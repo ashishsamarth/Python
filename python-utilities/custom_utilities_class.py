b7ce22e896bef67f97dd1ff425861308e2424575
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:22:06 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -238,11 +238,13 @@ class UsefulUtils:
         # Return type is a string
         return conditional.get(case)
 
-    # Method to join multiple string arguments into one concatenated string
-    # Arguments to this method: Multiple strings as arguments (separated by comma), user provided delimiter
-    # * takes care of the unpacking multiple tuples passed as arguments
-    # If no delimiter is provided, <space> is used as default value
     def concatenate_multiple_strs(*_inp_strs, case='original', delim='') -> str:
+        '''
+        Method to join multiple string arguments into one concatenated string
+        Arguments to this method: Multiple strings as arguments (separated by comma), user provided delimiter
+        * takes care of the unpacking multiple tuples passed as arguments
+        If no delimiter is provided, <space> is used as default value
+        '''
         # Return type is a string
         conditional = {'capitalize' : delim.join(_ for _ in (''.join(_ for _ in _inp_strs))).capitalize(),
                        'lower'      : delim.join(_ for _ in (''.join(_ for _ in _inp_strs))).lower(),
