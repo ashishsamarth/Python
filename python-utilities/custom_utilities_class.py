ac433f4be7ea4ed7aba7089842937efc80910278
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:10:46 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -69,9 +69,11 @@ class UsefulUtils:
         # In Ternary Operators: False Boolean is written first and True Boolean is written next
         return ( (False, True) [collections.Counter(_inp_str_1) == collections.Counter(_inp_str_2)])    
 
-    # Method to assign same values to provided keys and create a dictionary
-    # Arguments to this method: _keys as multiple list of elements, _value as any value
     def assign_same_val_to_all_keys(*_keys : list, _value) -> dict:
+        '''
+        Method to assign same values to provided keys and create a dictionary
+        Arguments to this method: _keys as multiple list of elements, _value as any value
+        '''
         # itertools.repeat : repeats the passed value seamlessly
         # zip : ties the keys and value together together
         # itertools.chain(*_keys) : Joins all lists to form a single list
