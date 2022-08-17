3ac22eaa7f2b7bab16ab4c0aa25e7064613ac079
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-17 11:28:46 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -603,10 +603,12 @@ class UsefulUtils:
         # Return type is a tuple
         return joined_tuple
 
-    # Method to join multiple lists into one list and keep only non-repeating values
-    # Argument to this method: Multiple lists as arguments (separated by comma)
-    # * takes care of the unpacking multiple lists passed as arguments
     def merge_lists(*_lists_iterables):
+        '''
+        Method to join multiple lists into one list and keep only non-repeating values
+        Argument to this method: Multiple lists as arguments (separated by comma)
+        * takes care of the unpacking multiple lists passed as arguments
+        '''
         # itertools.chain : joins the multiple lists into one
         # set removes the duplicate entries from the list
         # list converts the unique set to a list
