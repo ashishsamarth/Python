bf4300b3535bac6fb83dc019c02aae70ccc9cd5a
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 11:11:05 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -494,9 +494,11 @@ class UsefulUtils:
         # Return type is a string
         return ''.join(_ for _ in string.hexdigits if _.isupper())
 
-    # Method to get only upper case letters (A through Z)
-    # Argument to this method: None
     def get_upper_case_alphabets():
+        '''
+        Method to get only upper case letters (A through Z)
+        Argument to this method: None
+        '''
         # Return type is a string
         return string.ascii_uppercase
 
