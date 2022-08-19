5eda575fd626c798b92dd98b6467dbeed7248c99
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 10:59:22 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -464,9 +464,11 @@ class UsefulUtils:
         # Return type is a string
         return string.octdigits
 
-    # Method to get numbers 0 through 9 as string
-    # Argument to this method: None
     def get_str_0_9():
+        '''
+        Method to get numbers 0 through 9 as string
+        Argument to this method: None
+        '''
         # Return type is a string
         return string.digits
        
