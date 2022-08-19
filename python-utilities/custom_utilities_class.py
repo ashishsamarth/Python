50f312c65d53ea9d255a87104b7a6b9dca854ab1
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 10:53:18 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -406,11 +406,13 @@ class UsefulUtils:
         # Return type dependent on key
         return max_last_value
 
-    # Method to get length of input:
-    # This method is limited to basic data types (int, str, list, tuple, dict)
-    # Argument to this method: Input Variable
-    # Utilizes the length_hint function from the operator module
     def get_length_basic_dtypes(_inp):
+        '''
+        Method to get length of input:
+        This method is limited to basic data types (int, str, list, tuple, dict)
+        Argument to this method: Input Variable
+        Utilizes the length_hint function from the operator module
+        '''
         # Return type is an Integer
         return operator.length_hint(str(_inp)) if type(_inp) is int else operator.length_hint(_inp)
 
