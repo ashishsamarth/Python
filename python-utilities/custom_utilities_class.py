1a7d4a6f33dd726ea7dfca97e71e1bb5b7a82e34
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 11:09:22 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -536,9 +536,11 @@ class UsefulUtils:
         # Return type is a List
         return min_value
 
-    # Method to check if a string is a palindrome
-    # Argument to this method: string
     def is_str_a_palindrome(_inp_str):
+        '''
+        Method to check if a string is a palindrome
+        Argument to this method: string
+        '''
         reversed_string = _inp_str[::-1]
         return True if _inp_str == reversed_string else False
 
