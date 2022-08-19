0914b93e56785d24a774a5d389efe27a1ace98bf
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 10:58:33 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -456,9 +456,11 @@ class UsefulUtils:
         # returns: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
         return string.punctuation
 
-    # Method to get numbers 0 through 7 as string
-    # Argument to this method: None
     def get_str_0_7():
+        '''
+        Method to get numbers 0 through 7 as string
+        Argument to this method: None
+        '''
         # Return type is a string
         return string.octdigits
 
