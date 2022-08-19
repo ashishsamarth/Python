078598d2284fc0a2e1cc19e6441fea21a79c9bde
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-19 11:02:56 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -589,10 +589,12 @@ class UsefulUtils:
         # Return type is a dictionary
         return chained_dict
 
-    # Method to join multiple dictionaries to a single dictionary
-    # Argument to this method: Multiple Dictionaries as arguments (separated by comma)
-    # * takes care of the unpacking multiple dictionaries passed as arguments
     def join_dicts_m3(*_dicts_iterables):
+        '''
+        Method to join multiple dictionaries to a single dictionary
+        Argument to this method: Multiple Dictionaries as arguments (separated by comma)
+        * takes care of the unpacking multiple dictionaries passed as arguments
+        '''
         # Create an empty dictionary
         merged_dict = {}
         # Iterate over all the dictionaries in the unpacked list of dictionaries
