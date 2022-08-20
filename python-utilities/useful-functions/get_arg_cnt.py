4a8a551cd70f352c1e07c311ea5d7e1144a9eee2
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 10:02:44 -0500

    get_arg_cnt

--- python-utilities/useful-functions/get_arg_cnt.py
+++ python-utilities/useful-functions/get_arg_cnt.py
@@ -1,5 +1,7 @@
-# Method to get the number of arguments
-# Arguments are passed as a list, separated by comma
 def get_arg_cnt(*_arg):
+    '''
+    Method to get the number of arguments
+    Arguments are passed as a list, separated by comma
+    '''
     # Return type is an integer
     return len(_arg)
