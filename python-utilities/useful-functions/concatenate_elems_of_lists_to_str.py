2f4844bc94a1df702a7fc95cf75602c6bd0c3801
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 10:01:45 -0500

    concatenate_elems_of_lists_to_str

--- python-utilities/useful-functions/concatenate_elems_of_lists_to_str.py
+++ python-utilities/useful-functions/concatenate_elems_of_lists_to_str.py
@@ -1,9 +1,11 @@
-# Method to concatenate elements of multiple lists
-# Arguments to this method: Multiple lists as arguments (separated by comma), case of the result
-# * takes care of the unpacking multiple lists passed as arguments
-# Valid values for case are: 'capitalize', 'lower', 'original', 'swapcase', 'title', 'upper'
-# Default value of case is : original
-def concatenate_elems_of_lists_to_str(*_inp_list, case='original'):
+def concatenate_elems_of_lists_to_str(*_inp_list, case='original') -> str:
+    '''
+    Method to concatenate elements of multiple lists
+    Arguments to this method: Multiple lists as arguments (separated by comma), case of the result
+    * takes care of the unpacking multiple lists passed as arguments
+    Valid values for case are: 'capitalize', 'lower', 'original', 'swapcase', 'title', 'upper'
+    Default value of case is : original        
+    '''
     # Following is a dictionary with case values as keys and corresponding list comprehensions as their evaluated values
     # itertools.chain(*_inp_list) will join all the lists in to one list
     # Each element of the list will be joined using space as delimiter and then string function based on user selection are applied
