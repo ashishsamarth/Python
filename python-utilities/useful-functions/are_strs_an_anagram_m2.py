0993c93ea5a19b2912a618553ab6e9260f27da96
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 09:59:19 -0500

    are_strs_an_anagram_m2

--- python-utilities/useful-functions/are_strs_an_anagram_m2.py
+++ python-utilities/useful-functions/are_strs_an_anagram_m2.py
@@ -1,6 +1,8 @@
-# Method to check if two strings are anagrams
-# Argument to this method: two strings
-def are_strs_an_anagram_m2(_inp_str_1, _inp_str_2):
+def are_strs_an_anagram_m2(_inp_str_1: str, _inp_str_2: str) -> bool:
+    '''
+    Method to check if two strings are anagrams
+    Argument to this method: two strings
+    '''        
     # Return type is boolean
     # Utilizes Ternary Operator Approach
     # In Ternary Operators: False Boolean is written first and True Boolean is written next
