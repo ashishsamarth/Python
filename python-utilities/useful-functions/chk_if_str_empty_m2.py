d3d7fba62761643e0baa73bb86732e046c1fe905
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 10:00:02 -0500

    chk_if_str_empty_m2

--- python-utilities/useful-functions/chk_if_str_empty_m2.py
+++ python-utilities/useful-functions/chk_if_str_empty_m2.py
@@ -1,6 +1,8 @@
-# Method to check if given string is empty or not
-# Argument to this method: string
-def chk_if_str_empty_m2(_inp_str):
+def chk_if_str_empty_m2(_inp_str) -> bool:
+    '''
+    Method to check if given string is empty or not
+    Argument to this method: string
+    '''
     # Return type is boolean
     # Utilizes Ternary Operator Approach
     # In Ternary Operators: False Boolean is written first and True Boolean is written next
