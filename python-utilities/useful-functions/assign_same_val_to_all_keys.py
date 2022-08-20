cd84a94e10f8abd42698779c08b52446a41ed619
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 09:59:31 -0500

    assign_same_val_to_all_keys

--- python-utilities/useful-functions/assign_same_val_to_all_keys.py
+++ python-utilities/useful-functions/assign_same_val_to_all_keys.py
@@ -1,6 +1,8 @@
-# Method to assign same values to provided keys and create a dictionary
-# Arguments to this method: _keys as multiple list of elements, _value as any value
-def assign_same_val_to_all_keys(*_keys, _value):
+def assign_same_val_to_all_keys(*_keys : list, _value) -> dict:
+    '''
+    Method to assign same values to provided keys and create a dictionary
+    Arguments to this method: _keys as multiple list of elements, _value as any value
+    '''
     # itertools.repeat : repeats the passed value seamlessly
     # zip : ties the keys and value together together
     # itertools.chain(*_keys) : Joins all lists to form a single list
