af55ff438690f75c584169c6f2f6708ccb492058
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 10:00:46 -0500

    cnt_occurrence_of_all_elems_in_chained_lists

--- python-utilities/useful-functions/cnt_occurrence_of_all_elems_in_chained_lists.py
+++ python-utilities/useful-functions/cnt_occurrence_of_all_elems_in_chained_lists.py
@@ -1,7 +1,9 @@
-# Method to count occurrences of all elements in multiple lists
-# Arguments to this method: Multiple lists as arguments (separated by comma)
-# * takes care of the unpacking multiple lists passed as arguments
-def cnt_occurrence_of_all_elems_in_chained_lists(*_lists_iterable):
+def cnt_occurrence_of_all_elems_in_chained_lists(*_lists_iterable) -> dict:
+    '''
+    Method to count occurrences of all elements in multiple lists
+    Arguments to this method: Multiple lists as arguments (separated by comma)
+    * takes care of the unpacking multiple lists passed as arguments
+    '''
     # itertools.chain : joins the multiple lists into one
     chained_list = list(itertools.chain(*_lists_iterable))
     # Count the Occurrence of each value in chained list and keep value as key and occurrence count as value
