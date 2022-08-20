93fb6661d56296c64d2c833ae6d828b99d067605
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 10:01:15 -0500

    cnt_occurrence_of_elem_in_chained_lists

--- python-utilities/useful-functions/cnt_occurrence_of_elem_in_chained_lists.py
+++ python-utilities/useful-functions/cnt_occurrence_of_elem_in_chained_lists.py
@@ -1,7 +1,9 @@
-# Method to count occurrences of provided element in multiple lists
-# Arguments to this method: Multiple lists as arguments (separated by comma), element to be counted
-# * takes care of the unpacking multiple lists passed as arguments
-def cnt_occurrence_of_elem_in_chained_lists(*_lists_iterable, _element):
+def cnt_occurrence_of_elem_in_chained_lists(*_lists_iterable, _element) -> int:
+    '''
+    Method to count occurrences of provided element in multiple lists
+    Arguments to this method: Multiple lists as arguments (separated by comma), element to be counted
+    * takes care of the unpacking multiple lists passed as arguments
+    '''
     # itertools.chain : joins the multiple lists into one
     chained_list = list(itertools.chain(*_lists_iterable))
     # chained_list.count(_element) counts the number of occurrences, if will be triggered only the result is not zero
