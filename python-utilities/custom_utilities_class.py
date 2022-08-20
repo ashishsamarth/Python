dc2f457ccfbb39d56b47bb7e64151463a89a81c5
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-20 09:58:23 -0500

    custom_utilities_class

--- python-utilities/custom_utilities_class.py
+++ python-utilities/custom_utilities_class.py
@@ -276,20 +276,6 @@ class UsefulUtils:
         # Return type is a string
         return string.ascii_letters
 
-    def get_alphanum_from_strs(*_inp_strs, delim=''):
-        '''
-        Method to extract aplhabets and numbers from given list of strings
-        Argument to this method: Multiple strings as arguments (separated by comma), user provided delimiter
-        * takes care of the unpacking multiple strings passed as arguments
-        Default value for delimiter is ''
-        '''
-        # Internal for loop is calling the 'concatenate_multiple_strs' method in the same class
-        # Result of this method is a concatenated string
-        # The Outer for loop simply iterates over all the elements of the concatenated string and filter only alphabets and numbers
-        # These numerical values are then joined with user provided or default delimiter
-        # Return type is string
-        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if _.isalnum())
-
     def get_alphanum_and_underscore_from_strs(*_inp_strs, delim=''):
         '''
         Method to extract aplhabets, numbers and underscore from given list of strings
@@ -303,6 +289,20 @@ class UsefulUtils:
         # These numerical values are then joined with user provided or default delimiter
         # Return type is string
         return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if _.isalnum() or _ in ('_')) 
+
+    def get_alphanum_from_strs(*_inp_strs, delim=''):
+        '''
+        Method to extract aplhabets and numbers from given list of strings
+        Argument to this method: Multiple strings as arguments (separated by comma), user provided delimiter
+        * takes care of the unpacking multiple strings passed as arguments
+        Default value for delimiter is ''
+        '''
+        # Internal for loop is calling the 'concatenate_multiple_strs' method in the same class
+        # Result of this method is a concatenated string
+        # The Outer for loop simply iterates over all the elements of the concatenated string and filter only alphabets and numbers
+        # These numerical values are then joined with user provided or default delimiter
+        # Return type is string
+        return delim.join(_ for _ in UsefulUtils.concatenate_multiple_strs(*_inp_strs) if _.isalnum())
 
     def get_arg_cnt(*_arg):
         '''
