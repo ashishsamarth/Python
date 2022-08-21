d40f219969c463b70f5e37829ae6f82cf385528c
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-21 12:18:34 -0500

    get_str_0_9

--- python-utilities/useful-functions/get_str_0_9.py
+++ python-utilities/useful-functions/get_str_0_9.py
@@ -1,5 +1,7 @@
-# Method to get numbers 0 through 9 as string
-# Argument to this method: None
 def get_str_0_9():
+    '''
+    Method to get numbers 0 through 9 as string
+    Argument to this method: None
+    '''
     # Return type is a string
     return string.digits
