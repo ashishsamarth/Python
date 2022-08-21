a150f559f809342858802bbb10065cf68bcd20a5
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-21 12:20:51 -0500

    join_dicts_and_sort

--- python-utilities/useful-functions/join_dicts_and_sort.py
+++ python-utilities/useful-functions/join_dicts_and_sort.py
@@ -1,11 +1,13 @@
-# Method to join multiple dictionaries to a single dictionary and sort the new dictionary based on keys or values in
-# Ascending or Descending order
-# Argument to this method: Multiple Dictionaries as arguments (separated by comma), sorting key and sorting type
-# * takes care of the unpacking multiple dictionaries passed as arguments
-# _sort_key='key', _sort_reversal=False is defaulted to sorting by key in ascending order
-# _sort_key='value' will sort the resulting dictionary by values in ascending order
-# _sort_key='value', _sort_reversal=True will sort the resulting dictionary by values in descending order
 def join_dicts_and_sort(*_dicts_iterables, _sort_key='key', _sort_reversal=False):
+    '''
+    Method to join multiple dictionaries to a single dictionary and sort the new dictionary based on keys or value
+    Ascending or Descending order
+    Argument to this method: Multiple Dictionaries as arguments (separated by comma), sorting key and sorting type
+    * takes care of the unpacking multiple dictionaries passed as arguments
+    _sort_key='key', _sort_reversal=False is defaulted to sorting by key in ascending order
+    _sort_key='value' will sort the resulting dictionary by values in ascending order
+    _sort_key='value', _sort_reversal=True will sort the resulting dictionary by values in descending order
+    '''
     # Create an empty dictionary
     merged_dict = {}
     # Iterate over all the dictionaries in the unpacked list of dictionaries
