6a59a844d85c1fee98757595b28f7059b35252df
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-21 12:20:35 -0500

    is_str_a_palindrome

--- python-utilities/useful-functions/is_str_a_palindrome.py
+++ python-utilities/useful-functions/is_str_a_palindrome.py
@@ -1,5 +1,7 @@
-# Method to check if a string is a palindrome
-# Argument to this method: string
 def is_str_a_palindrome(_inp_str):
+    '''
+    Method to check if a string is a palindrome
+    Argument to this method: string
+    '''
     reversed_string = _inp_str[::-1]
     return True if _inp_str == reversed_string else False
