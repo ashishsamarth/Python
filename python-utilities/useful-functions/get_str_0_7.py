510430e4258f3d4d796aea5fbb3367f34092b3dc
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-21 12:18:24 -0500

    get_str_0_7

--- python-utilities/useful-functions/get_str_0_7.py
+++ python-utilities/useful-functions/get_str_0_7.py
@@ -1,5 +1,7 @@
-# Method to get numbers 0 through 7 as string
-# Argument to this method: None
 def get_str_0_7():
+    '''
+    Method to get numbers 0 through 7 as string
+    Argument to this method: None
+    '''
     # Return type is a string
     return string.octdigits
