0e917ad0db922a4d5c552e8a45383a7e7ce8ec33
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:31:14 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -936,10 +936,12 @@ class CustomOpenpyxl:
                     sorted([_ for _ in _val.split(_delim)]))
         self.save_wb()
 
-    # Method to sort values in a given row in an iterative manner
-    # Arguments to this method are: - Index of column, row number of header, delimiter the values are separated by
-    # and delimiter value by which the values will be joined by
     def sort_dsc_row_val_in_col_sep_by_delim_active_ws(self, _header_row_num, _col_name, _delim, _join_by_delim):
+        '''
+        Method to sort values in a given row in an iterative manner
+        Arguments to this method are: - Index of column, row number of header, delimiter the values are separated by
+        and delimiter value by which the values will be joined by
+        '''
         _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
         for _idx, _val in enumerate(self.get_specific_col_val_by_col_name_in_active_ws(_header_row_num, _col_name),
                                     start=1):
