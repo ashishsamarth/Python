06f87860bf5086c5b530f321e806aa766fc5828b
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:29:34 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -963,9 +963,11 @@ class CustomOpenpyxl:
     def save_wb(self):
         return self.my_base_wb.save(self.my_filename)
 
-    # Method to save the workbook to new file
-    # Argument for this method is: - New File Name
     def save_wb_as_new_file(self, _new_file_name):
+        '''
+        Method to save the workbook to new file
+        Argument for this method is: - New File Name
+        '''
         assert _new_file_name.split('.')[-1] in ['xls', 'xlsx'], 'Input file does not have valid excel extension'
         return self.my_base_wb.save(_new_file_name)
 
