ac4d73f93517c965db0eaa2072259fc8b8776c30
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:15:42 -0500

    s3_list_buckets

--- python-aws-boto3/AWS-Boto3-S3/useful-functions/s3_list_buckets.py
+++ python-aws-boto3/AWS-Boto3-S3/useful-functions/s3_list_buckets.py
@@ -1,6 +1,8 @@
-# Method to list S3 buckets on the user account
-# Argument to this method is: None
-def s3_list_buckets(self):
+def s3_list_buckets(self) -> list:
+    '''
+    Method to list S3 buckets on the user account
+    Argument to this method is: None
+    '''
     # Using List comprehension, pull the bucket names from the response of list_buckets method
     buckets = [_['Name'] for _ in self.s3_client.list_buckets()['Buckets']]
     # return type of this method is a list
