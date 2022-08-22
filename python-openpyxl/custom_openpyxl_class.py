da54df4cb554e375943ca873db16e179613aef58
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:28:26 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -969,9 +969,11 @@ class CustomOpenpyxl:
         assert _new_file_name.split('.')[-1] in ['xls', 'xlsx'], 'Input file does not have valid excel extension'
         return self.my_base_wb.save(_new_file_name)
 
-    # Method to write static value to empty cells in a given column
-    # Arguments to this method are:- Row number of the header wow, Column name and static to be written
     def write_values_to_empty_cells_in_column(self, _header_row_num, _col_name, _write_val):
+        '''
+        Method to write static value to empty cells in a given column
+        Arguments to this method are:- Row number of the header wow, Column name and static to be written
+        '''
         # Fetch the column index from column name using 'ref_col_name_letter_map' method
         _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
         for _row_idx in self.get_row_idx_lst_of_empty_rows_specific_col(_header_row_num, _col_name):
