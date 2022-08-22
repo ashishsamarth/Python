9a72e720f3bc440fa2c46d6c80720115c0bf8a54
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:10:10 -0500

    custom_AWS_Boto3_S3_Class

--- python-aws-boto3/AWS-Boto3-S3/custom_AWS_Boto3_S3_Class.py
+++ python-aws-boto3/AWS-Boto3-S3/custom_AWS_Boto3_S3_Class.py
@@ -66,9 +66,11 @@ class AwsBoto3S3:
             if error_data.response['Error']['Code'] in AwsBoto3S3.S3_Error_Map.keys():
                 print(str(_bucket_name) + ':- ' + AwsBoto3S3.S3_Error_Map[error_data.response['Error']['Code']])
 
-    # Method to copy file from one data bucket to another
-    # Arguments to this method are:- Source-Bucket-Name, Source-File-Name, Target-Bucket-Name, Target-File-Name
     def s3_bucket_copy_file_from_one_bucket_to_another(self, _src_bucket_name, _src_file_name, _tgt_bucket_name, _tgt_file_name):
+        '''
+        Method to copy file from one data bucket to another
+        Arguments to this method are:- Source-Bucket-Name, Source-File-Name, Target-Bucket-Name, Target-File-Name
+        '''
         # Special Note: We have to use s3_resource instead of s3_client
         # capture the source bucket name and source file name in 'copy_source'
         # copy_source should not be modified, since its part of syntax
