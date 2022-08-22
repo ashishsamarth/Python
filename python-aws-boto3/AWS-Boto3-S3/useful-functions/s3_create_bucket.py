161c8201c22b7d5698743b8f83843d3ff6b20c16
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-22 15:14:30 -0500

    s3_create_bucket

--- python-aws-boto3/AWS-Boto3-S3/useful-functions/s3_create_bucket.py
+++ python-aws-boto3/AWS-Boto3-S3/useful-functions/s3_create_bucket.py
@@ -1,19 +1,9 @@
-import boto3
-from botocore.exceptions import ClientError
-
-class AwsBoto3S3:
-
-    AWS_REGION = {'loc_1': 'us-east-1'}
-
-    S3_Error_Map = {'InvalidBucketName': 'BucketName does not align with S3 naming convention',
-                    'BucketAlreadyExists': 'The requested bucket name is not available.',
-                    'IllegalLocationConstraintException': 'The unspecified location constraint is incompatible for the region',
-                    'NoSuchBucket': 'The specified bucket does not exist'}
-    
-# Method to create a S3 Bucket
-# Argument to this method is:- Bucket Name
-# Note: S3 bucket names must be globally unique amongst all AWS customers
 def s3_create_bucket(self, _bucket_name):
+    '''
+    Method to create a S3 Bucket
+    Argument to this method is:- Bucket Name
+    Note: S3 bucket names must be globally unique amongst all AWS customers
+    '''
     try:
         # Call the create_bucket method on the object
         execute = self.s3_client.create_bucket(Bucket=_bucket_name)
@@ -22,4 +12,4 @@ def s3_create_bucket(self, _bucket_name):
     # In case code encounters common error, a brief message of the error will be presented to the user
     except ClientError as error_data:
         if error_data.response['Error']['Code'] in AwsBoto3S3.S3_Error_Map.keys():
-            print(str(_bucket_name) + ':- ' + AwsBoto3S3.S3_Error_Map[error_data.response['Error']['Code']])
+            print(str(_bucket_name) + ':- ' + AwsBoto3S3.S3_Error_Map[error_data.response['Error']['Code']]) 
