febf816d78e3a9c831853e7f39c342ce31496984
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-23 07:52:34 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -830,9 +830,11 @@ class CustomOpenpyxl:
         # Return type is list
         return _row_idx_list
 
-    # Method to get row indexes of empty rows in a specific column
-    # Arguments to this method are: - Row number of header row and Column name
     def get_row_idx_lst_of_empty_rows_specific_col(self, _header_row_num, _col_name):
+        '''
+        Method to get row indexes of empty rows in a specific column
+        Arguments to this method are: - Row number of header row and Column name
+        '''
         # Fetch the column index from column name using 'ref_col_name_idx_map' method
         _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
         # Get the list of indexes of empty rows excluding the header row
