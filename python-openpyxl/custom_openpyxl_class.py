bd1761b3b63e276159f143a179d13a8c63c1ce60
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-23 08:01:34 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -319,9 +319,11 @@ class CustomOpenpyxl:
             _cell_header.border = self.thick_border
         self.save_wb()
 
-    # Method to copy and paste data from one column to another, excluding header
-    # Arguments to this method are: - name of source column, name of target column & row number for header
     def copy_and_paste_column_values_by_col_name(self, _src_col_name, _tgt_col_name, _header_row_num):
+        '''
+        Method to copy and paste data from one column to another, excluding header
+        Arguments to this method are: - name of source column, name of target column & row number for header
+        '''
         # Fetch the column index from column name using 'ref_col_name_letter_map' method
         _src_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_src_col_name])
         _tgt_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_tgt_col_name])
