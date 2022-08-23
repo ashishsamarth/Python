a92aa21839ba34cb80bb5aac4686a0255798401f
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-23 07:56:27 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -214,9 +214,11 @@ class CustomOpenpyxl:
                     # This method calls save internally, hence its auto save from user perspective
                     self.save_wb()
 
-    # Method to create a new worksheet in workbook
-    # Argument to this method is: - New worksheet name
     def add_new_ws_at_end_of_wb(self, _new_ws_name):
+        '''
+        Method to create a new worksheet in workbook
+        Argument to this method is: - New worksheet name
+        '''
         # New worksheet will be added to workbook, only if it doesn't exist already
         if not self.if_ws_in_wb(_new_ws_name):
             self.my_base_wb.create_sheet(_new_ws_name)
