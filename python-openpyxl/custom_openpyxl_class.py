64b4c35a61c6aa1d88d422d476ec4360a12a81a2
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-24 21:12:17 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -102,7 +102,7 @@ class CustomOpenpyxl:
         # Return type is dictionary
         return col_idx_name_map
 
-    def ref_col_name_idx_map(self, _header_row_num) -> dict:
+    def ref_col_name_idx_map(self, _header_row_num: int) -> dict:
         '''
         Method to get a map of column name and column index
         Argument to this method is: - Row number of header row
