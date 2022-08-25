53b414207d6db34ba5134781200a0f955e969ad8
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-24 21:23:28 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -422,17 +422,15 @@ class CustomOpenpyxl:
                 filled_cells.fill = _my_color
         self.save_wb()
 
-    def change_row_color_by_cell_value_near_match(self, _header_row_num, _col_name, _cell_value, _fill_color):
+    def change_row_color_by_cell_value_near_match(self, _header_row_num: int, _col_name: str, _cell_value, _fill_color: str):
         '''
         Method to change row color based on a near match of cell value in particular column
         Arguments to this method: - Row number for header row, Column name, value of cell, user selected fill color
         '''
         # Get the list of indexes where near match of searched value if found excluding the header row
-        _row_idx = self.get_row_idx_lst_based_on_search_val_specific_col_near_match(_header_row_num, _col_name,
-                                                                                    _cell_value)
+        _row_idx = self.get_row_idx_lst_based_on_search_val_specific_col_near_match(_header_row_num, _col_name, _cell_value)
         # Set the color fill based on user selected color
-        _my_color = PatternFill(start_color=self._my_color_map[_fill_color], end_color=self._my_color_map[_fill_color],
-                                fill_type='solid')
+        _my_color = PatternFill(start_color=self._my_color_map[_fill_color], end_color=self._my_color_map[_fill_color], fill_type='solid')
         # Iterate over the list of row indexes identified earlier
         for _row_num in _row_idx:
             # Iterate over each column of the row
