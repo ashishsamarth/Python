0e233dc862077dcdea533f4b125681a6f55cf477
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-25 21:10:41 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -645,7 +645,7 @@ class CustomOpenpyxl:
         # Save the workbook
         self.save_wb()
 
-    def filter_rows_by_val_in_any_col_in_active_ws(self, _header_row_num, _filter_val):
+    def filter_rows_by_val_in_any_col_in_active_ws(self, _header_row_num: int, _filter_val):
         '''
         Method to filter a row based on a value present in any column of worksheet
         Argument to this method is:- value to be used as filter
