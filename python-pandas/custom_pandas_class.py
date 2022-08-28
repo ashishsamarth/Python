8e531aaa7042e3db5f7880659b9f93008ddfbb58
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-27 22:16:09 -0500

    custom_pandas_class

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -421,7 +421,7 @@ class CustomPandas:
 
     # Method to add a delimiter as suffix
     # Note:- This was added for understanding, however their are pre-built functions in pandas for this task
-    def suffix_delim(self, _tgt_col_name, _delim):
+    def suffix_delim(self, _tgt_col_name: str, _delim: str):
         self.my_df[_tgt_col_name] = self.my_df[_tgt_col_name].astype(str) + _delim
 
     # Method to save the dataframe in the same file
