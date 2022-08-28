7d361fd5717f714509da7c3d61430feb1f6299e6
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-27 22:12:32 -0500

    custom_pandas_class

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -114,7 +114,7 @@ class CustomPandas:
 
     # Method to create dataframe with based on one condition at column level
     # Arguments to this method are: - Column name where condition is to be applied, and the condition itself
-    def create_df_from_filtered_data_single_in_condition(self, _column_name, _condition):
+    def create_df_from_filtered_data_single_in_condition(self, _column_name: str, _condition):
         # create a new filtered dataset based on the condition applied on the column
         _filtered_data_set = (self.my_df.loc[self.my_df[_column_name] == _condition])
         # return Dataframe
