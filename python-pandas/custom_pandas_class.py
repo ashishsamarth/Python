bc37731dbc04232108166b0b13de64259bae7bbb
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-27 22:15:15 -0500

    custom_pandas_class

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -445,7 +445,7 @@ class CustomPandas:
 
     # Method to set a column as the index column
     # Argument to this method is: - Column name which needs to be set as index
-    def set_col_as_index(self, _col_name):
+    def set_col_as_index(self, _col_name: str):
         return self.my_df.set_index(_col_name)
 
     # Method to get frequency of occurrences per unique item in a given column
