193afc9df2d3f278c3cc8137ea614cf2cb6ec79f
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-28 22:07:04 -0500

    custom_pandas_class

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -424,9 +424,11 @@ class CustomPandas:
     def suffix_delim(self, _tgt_col_name: str, _delim: str):
         self.my_df[_tgt_col_name] = self.my_df[_tgt_col_name].astype(str) + _delim
 
-    # Method to save the dataframe in the same file
-    # Based on the file extension appropriate pandas function will be called to save the data
     def save_df_same_tgt(self):
+        '''
+        Method to save the dataframe in the same file
+        Based on the file extension appropriate pandas function will be called to save the data
+        '''
         # If the file extension is xlsx then 'to_excel' will be called
         if self.my_filename.split('.')[-1] == 'xlsx':
             return self.my_df.to_excel(self.my_filename, index=False)
