fe5388df2127ce783e01364b1c14063c68640587
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-28 22:11:57 -0500

    custom_pandas_class

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -384,9 +384,11 @@ class CustomPandas:
     def get_unique_value_count_in_col(self, _col_name):
         return self.my_df[_col_name].nunique()
 
-    # Method to add a delimiter as prefix
-    # Note:- This was added for understanding, however their are pre-built functions in pandas for this task
     def prefix_delim(self, _tgt_col_name, _delim):
+        '''
+        Method to add a delimiter as prefix
+        Note:- This was added for understanding, however their are pre-built functions in pandas for this task
+        '''
         self.my_df[_tgt_col_name] = _delim + self.my_df[_tgt_col_name].astype(str)
 
     def reset_to_default_int_idx(self):
