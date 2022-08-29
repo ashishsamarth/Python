08f21980dab6dd2513f5def99a90f98f4b31359d
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-29 07:55:36 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -545,6 +545,21 @@ class CustomOpenpyxl:
             _my_dict[_] = [_row[1] for _row in self.my_base_active_ws.values if _row[0] == _]
         return _my_dict
 
+    def create_new_wb(self, _file_location, _wb_name, _ws_name):
+        '''
+        Method to create a new workbook using an exsting instantiated object
+        Arguments to this Method: Destination Directory, Workbok name, Worksheet name
+        '''
+        # Create a workbook object
+        _wb = openpyxl.Workbook()
+        # Workbook will have atleast one worksheet
+        _ws = _wb.active
+        # Rename the worksheet with the name provided as an argument
+        _ws.title = _ws_name
+        # Save the newly created workbook
+        _wb.save(filename=_file_location+_wb_name)
+        return 'f New Workbook created with name:- {_wb_name}'
+
     def df_to_rows(self, _dataframe, _ws_name: str):
         '''
         Method to convert dataframe to excel rows
