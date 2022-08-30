09bdf5a8a83307f5b49943f3fdd20fb2d01e36d6
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-29 21:03:41 -0500

    db_session_close

--- python-cassandra/useful-functions/db_session_close.py
+++ python-cassandra/useful-functions/db_session_close.py
@@ -1,8 +1,8 @@
 # 
-# Call the shutdown method on the session
+# 
 def db_session_close(self):
     '''
     Method to close the session
-    
+    Call the shutdown method on the session
     '''
     self.db_session().shutdown()
