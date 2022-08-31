7ea9a84ac3c6fd14a5325e9f8d4e1a8c556c0842
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-30 22:15:49 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -73,8 +73,11 @@ class CustomFileHandler:
             elif in_newline.lower() in ['n', 'no']:
                 write_mode.write(data_set)
 
-     # Method to write user provided text to a file in single line followed by a new line character
+     # 
     def file_write_mode_write_line(self, _text, in_newline):
+        '''
+        Method to write user provided text to a file in single line followed by a new line character
+        '''
         with open(self.my_filename, 'w') as write_mode:
             # Based on the value provided for argument in_newline the text will be written with or without newline
             if in_newline.lower() in ['y', 'yes']:
