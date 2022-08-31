19d239e1b3b85073dedc9d5481c36fd1286d2676
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-31 08:57:50 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -419,23 +419,6 @@ class CustomOpenpyxl:
                 filled_cells.fill = _my_color
         self.save_wb()
 
-    def copy_and_paste_column_values_by_col_name(self, _src_col_name: str, _tgt_col_name: str, _header_row_num: int):
-        '''
-        Method to copy and paste data from one column to another, excluding header
-        Arguments to this method are: - name of source column, name of target column & row number for header
-        '''
-        # Fetch the column index from column name using 'ref_col_name_letter_map' method
-        _src_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_src_col_name])
-        _tgt_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_tgt_col_name])
-        # Enumerate over values (by rows) of the worksheet
-        for _xl_row_idx, _row_val in enumerate(self.my_base_active_ws.iter_rows(values_only=True), start=1):
-            # Exclude the header row
-            if _xl_row_idx != _header_row_num:
-                # Write the contents of cells to target column from source column
-                self.my_base_active_ws.cell(row=_xl_row_idx, column=_tgt_col_idx).value = self.my_base_active_ws.cell(row=_xl_row_idx, column=_src_col_idx).value
-        # Save workbook
-        self.save_wb()
-
     def change_row_color_non_empty_cell_in_col(self, _header_row_num: int, _col_name: str, _fill_color: str):
         '''
         Method to change row color based on a non-empty cell in particular column
@@ -458,6 +441,23 @@ class CustomOpenpyxl:
                 filled_cells = self.my_base_active_ws.cell(row=_row_num, column=_)
                 # apply color
                 filled_cells.fill = _my_color
+        self.save_wb()
+
+    def copy_and_paste_column_values_by_col_name(self, _src_col_name: str, _tgt_col_name: str, _header_row_num: int):
+        '''
+        Method to copy and paste data from one column to another, excluding header
+        Arguments to this method are: - name of source column, name of target column & row number for header
+        '''
+        # Fetch the column index from column name using 'ref_col_name_letter_map' method
+        _src_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_src_col_name])
+        _tgt_col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_tgt_col_name])
+        # Enumerate over values (by rows) of the worksheet
+        for _xl_row_idx, _row_val in enumerate(self.my_base_active_ws.iter_rows(values_only=True), start=1):
+            # Exclude the header row
+            if _xl_row_idx != _header_row_num:
+                # Write the contents of cells to target column from source column
+                self.my_base_active_ws.cell(row=_xl_row_idx, column=_tgt_col_idx).value = self.my_base_active_ws.cell(row=_xl_row_idx, column=_src_col_idx).value
+        # Save workbook
         self.save_wb()
 
     def copy_src_col_paste_tgt_col_diff_sheets(self, _header_row_num: int, _src_ws_name: str, _src_col_name: str, _tgt_ws_name: str, _tgt_col_name: str):
