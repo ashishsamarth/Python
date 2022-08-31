05eb4ce2401e28cc674a5b34d9a7ff245d333be6
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-31 08:07:05 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -878,6 +878,22 @@ class CustomOpenpyxl:
         except AssertionError:
             print('AssertionError: Column Letter Outside Data-Set Range')
 
+    def get_specific_col_val_as_str_by_col_letter_in_active_ws(self, _header_row_num, _col_letter) -> str:
+        '''
+        Method to get column values for a given column letter
+        Arguments to this method are:- Row number for header row & column letter
+        '''
+        assert _col_letter in self.ref_col_letter_name_map(
+            _header_row_num).keys(), 'Column Letter Outside Data-Set Range'
+        _col_idx = column_index_from_string(_col_letter)
+        try:
+            return [str(_col_value[0]) for _col_value in self.my_base_active_ws.iter_rows(min_row=_header_row_num + 1,
+                                                                                     min_col=_col_idx,
+                                                                                     max_col=_col_idx,
+                                                                                     values_only=True)]
+        except AssertionError:
+            print('AssertionError: Column Letter Outside Data-Set Range')
+
     def get_row_idx_lst_based_on_search_val_specific_col_near_match(self, _header_row_num: int, _col_name: str, _search_val):
         '''
         Method to get row indexes of searched values in a specific column with near match condition
