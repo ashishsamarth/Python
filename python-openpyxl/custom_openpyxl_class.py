b8855fc085622cb33a3f8d955dcc79bc4b91206d
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-31 08:01:34 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -836,6 +836,7 @@ class CustomOpenpyxl:
         # Check if provided column name exists in the worksheet
         if _col_name in self.ref_col_idx_name_map(_header_row_num).values():
             # Fetch the values from the column provdided
+            # Skip the header row
 
 
     def get_specific_col_val_by_col_idx_in_active_ws(self, _header_row_num: int, _col_idx: int):
