8e55ee19ce7ffd901036ca5dd40ccc5bd9b859c3
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-30 22:16:30 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -84,8 +84,11 @@ class CustomFileHandler:
             elif in_newline.lower() in ['n', 'no']:
                 write_mode.writelines(_text)
 
-    # Method to truncate the file using the append mode
+    # 
     def file_truncate_mode(self):
+        '''
+        Method to truncate the file using the append mode
+        '''
         with open(self.my_filename, 'a') as truncate_mode:
             truncate_mode.truncate(0)
 
