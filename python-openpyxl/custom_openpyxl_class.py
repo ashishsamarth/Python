bd9122049eff21336f6289fe700c7d0c28a98ae3
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-31 09:00:49 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -467,6 +467,20 @@ class CustomOpenpyxl:
         # Save workbook
         self.save_wb()
 
+    def copy_header_row_src_ws_to_tgt_ws(self, _header_row_num: int, _src_ws_name: str, _tgt_ws_name: str):
+        '''
+        Method to copy the header value from one worksheet to another
+        Arguments to this method are: Row number of the header row, source worksheet name and target worksheet name  
+        '''
+        # Capture the column names from the source worksheet names using list comprehension
+        _src_col_name = [_ for _ in self.my_base_wb[_src_ws_name].iter_rows(min_row=_header_row_num, max_row=(_header_row_num), values_only=True)][0]
+        # Iterate over the column names and the column indexes starting at position = 1
+        for _col_idx, _col_val in enumerate(_src_col_name, start=1):
+            # Write the column name to the target worksheet based on the iterated index and value
+            self.my_base_wb[_tgt_ws_name].cell(row=_header_row_num, column=_col_idx).value = _col_val
+        # Save the workbook
+        self.save_wb()
+
     def copy_src_col_paste_tgt_col_diff_sheets(self, _header_row_num: int, _src_ws_name: str, _src_col_name: str, _tgt_ws_name: str, _tgt_col_name: str):
         '''
         Method to copy data from source and paste it to target of different worksheets in same workbook
@@ -515,20 +529,6 @@ class CustomOpenpyxl:
                     self.my_base_wb[_tgt_ws].cell(row=_row, column=_col).value = self.my_base_wb[_src_ws].cell(row=_row,
                                                                                                                column=_col).value
             self.save_wb()
-
-    def copy_header_row_src_ws_to_tgt_ws(self, _header_row_num: int, _src_ws_name: str, _tgt_ws_name: str):
-        '''
-        Method to copy the header value from one worksheet to another
-        Arguments to this method are: Row number of the header row, source worksheet name and target worksheet name  
-        '''
-        # Capture the column names from the source worksheet names using list comprehension
-        _src_col_name = [_ for _ in self.my_base_wb[_src_ws_name].iter_rows(min_row=_header_row_num, max_row=(_header_row_num), values_only=True)][0]
-        # Iterate over the column names and the column indexes starting at position = 1
-        for _col_idx, _col_val in enumerate(_src_col_name, start=1):
-            # Write the column name to the target worksheet based on the iterated index and value
-            self.my_base_wb[_tgt_ws_name].cell(row=_header_row_num, column=_col_idx).value = _col_val
-        # Save the workbook
-        self.save_wb()
 
     def create_dict_from_values(self):
         '''
