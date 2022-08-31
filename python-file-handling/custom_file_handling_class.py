11d129608cdc5ee99de95c88d5b1a40676743263
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-30 22:14:46 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -52,8 +52,10 @@ class CustomFileHandler:
             else:
                 print(single_line)
 
-    # Method to read  the characters from the file based on user input
     def file_read_mode_read_char(self, num_of_characters=5):
+        '''
+        Method to read  the characters from the file based on user input
+        '''
         with open(self.my_filename, 'rt', encoding='utf8') as read_char:
             # The default value of argument num_of_characters is set to 5, in case the user does not provide the value
             # Meaning if the method is called without passing the argument, the method will return first 5 characters from the file
