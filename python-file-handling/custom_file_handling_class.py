66da91270eadc986009a5c8a872c384d93a5bc8b
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-30 22:12:43 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -14,7 +14,9 @@ class CustomFileHandler:
         self.my_filename = _my_file_name
 
     def get_file_extension(self):
-        # Method to get the file extension using the split method of class <str>
+        '''
+        Method to get the file extension using the split method of class <str>
+        '''
         my_file_extension = self.my_filename.split('.')[-1]
         # This will only return the result and does not perform printing
         return my_file_extension
