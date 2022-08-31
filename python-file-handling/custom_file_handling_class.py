d9f138ea523baacb14064860dfd563aa642dc8b9
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-30 22:17:12 -0500

    custom_file_handling_class

--- python-file-handling/custom_file_handling_class.py
+++ python-file-handling/custom_file_handling_class.py
@@ -91,8 +91,11 @@ class CustomFileHandler:
         with open(self.my_filename, 'a') as truncate_mode:
             truncate_mode.truncate(0)
 
-    # Method to append the text to file
+    # 
     def file_append_mode(self, data_set, in_newline):
+        '''
+        Method to append the text to file
+        '''
         # open the filename captured during initialization in append mode
         with open(self.my_filename, 'a') as append_mode:
             # Based on the value provided for argument in_newline the text will be written with or without newline
