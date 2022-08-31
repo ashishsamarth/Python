72026d35260c88f65c40fbc4c7807a3d8c394f58
ashishsamarth <mrashishsamarth@gmail.com>
2022-08-31 08:10:57 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -317,6 +317,26 @@ class CustomOpenpyxl:
             _cell_header.border = self.thick_border
         self.save_wb()
 
+    def change_cell_color_by_cell_value_exact_match(self, _header_row_num: int, _col_name: str, _cell_value, _fill_color: str):
+        '''
+        Method to change row color based on an exact match of cell value in particular column
+        Arguments to this method: - Row number for header row, Column name, value of cell, user selected fill color
+        '''
+        # Fetch the column index from column name using 'ref_col_name_letter_map' method
+        _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
+        # Get the list of indexes where near match of searched value if found excluding the header row
+        _row_idx = self.get_row_idx_lst_based_on_search_val_specific_col_exact_match(_header_row_num, _col_name, _cell_value)
+        # Set the color fill based on user selected color
+        _my_color = PatternFill(start_color=self._my_color_map[_fill_color], end_color=self._my_color_map[_fill_color], fill_type='solid')
+        # Iterate over the list of row indexes identified earlier
+        for _row_num in _row_idx:
+            # Identify cell range with matching criteria
+            # Note: The updated index value is to be used since we are writing to the cell
+            filled_cells = self.my_base_active_ws.cell(row=_row_num, column=_col_idx)
+            # set the color fill for row
+            filled_cells.fill = _my_color
+        self.save_wb()
+
     def copy_and_paste_column_values_by_col_name(self, _src_col_name: str, _tgt_col_name: str, _header_row_num: int):
         '''
         Method to copy and paste data from one column to another, excluding header
@@ -343,26 +363,6 @@ class CustomOpenpyxl:
         _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
         # Get the list of indexes where near match of searched value if found excluding the header row
         _row_idx = self.get_row_idx_lst_based_on_search_val_specific_col_near_match(_header_row_num, _col_name, _cell_value)
-        # Set the color fill based on user selected color
-        _my_color = PatternFill(start_color=self._my_color_map[_fill_color], end_color=self._my_color_map[_fill_color], fill_type='solid')
-        # Iterate over the list of row indexes identified earlier
-        for _row_num in _row_idx:
-            # Identify cell range with matching criteria
-            # Note: The updated index value is to be used since we are writing to the cell
-            filled_cells = self.my_base_active_ws.cell(row=_row_num, column=_col_idx)
-            # set the color fill for row
-            filled_cells.fill = _my_color
-        self.save_wb()
-
-    def change_cell_color_by_cell_value_exact_match(self, _header_row_num: int, _col_name: str, _cell_value, _fill_color: str):
-        '''
-        Method to change row color based on an exact match of cell value in particular column
-        Arguments to this method: - Row number for header row, Column name, value of cell, user selected fill color
-        '''
-        # Fetch the column index from column name using 'ref_col_name_letter_map' method
-        _col_idx = column_index_from_string(self.ref_col_name_letter_map(_header_row_num)[_col_name])
-        # Get the list of indexes where near match of searched value if found excluding the header row
-        _row_idx = self.get_row_idx_lst_based_on_search_val_specific_col_exact_match(_header_row_num, _col_name, _cell_value)
         # Set the color fill based on user selected color
         _my_color = PatternFill(start_color=self._my_color_map[_fill_color], end_color=self._my_color_map[_fill_color], fill_type='solid')
         # Iterate over the list of row indexes identified earlier
