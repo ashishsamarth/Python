a51d9002070a262fc90e6594cc29933d32fee77a
Ashish Samarth <mrashishsamarth@gmail.com>
2022-09-01 09:18:21 -0500

    custom_openpyxl_class

--- python-openpyxl/custom_openpyxl_class.py
+++ python-openpyxl/custom_openpyxl_class.py
@@ -234,8 +234,7 @@ class CustomOpenpyxl:
         # Enumerate over the values from 'get_specific_col_val_by_col_name_in_active_ws' method
         # start with index of (header_row+1) since we need to add prefix values to column values only not column header
         # This updated index start value is used for the row for enumeration
-        for _idx, _val in enumerate(self.get_specific_col_val_by_col_name_in_active_ws(_header_row_num, _col_name),
-                                    start=_header_row_num + 1):
+        for _idx, _val in enumerate(self.get_specific_col_val_by_col_name_in_active_ws(_header_row_num, _col_name), start=_header_row_num + 1):
             # LHS: Assign values to each cell in the column
             # RHS: Concatenate existing value of the cell with the prefixed value
             self.my_base_active_ws.cell(row=_idx, column=_col_idx).value = (
