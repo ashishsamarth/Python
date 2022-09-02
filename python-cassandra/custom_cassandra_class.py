1bd658a68742b5ed112f88d12dfcd79b4c9e9c3e
ashish samarth <mrashishsamarth@gmail.com>
2022-09-01 21:07:54 -0500

    custom_cassandra_class

--- python-cassandra/custom_cassandra_class.py
+++ python-cassandra/custom_cassandra_class.py
@@ -89,9 +89,13 @@ class CustomCassandra:
         # return type of this method is a dictionary
         return row_result_as_dict
 
-    # Method to execute a cql query
-    # Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+    # 
+    # 
     def exec_select_return_dict_fetch_all(self, _query):
+        '''
+        Method to execute a cql query
+        Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+        '''
         session = self.db_session()
         # This line sets up the return type of the query as a dictionary
         session.row_factory = dict_factory
