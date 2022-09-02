5d4a555b05e8036fab1d5b5b496ae593332ad449
ashish samarth <mrashishsamarth@gmail.com>
2022-09-01 21:04:14 -0500

    custom_cassandra_class

--- python-cassandra/custom_cassandra_class.py
+++ python-cassandra/custom_cassandra_class.py
@@ -110,7 +110,6 @@ class CustomCassandra:
         return row_result_as_ordered_dict
 
     # 
-    # 
     def exec_select_return_ordered_dict_fetch_all(self, _query):
         '''
         Method to execute a cql query
