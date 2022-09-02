12417ae9e23c0aa5a9cc2f1e2568a7cbd3f12320
ashish samarth <mrashishsamarth@gmail.com>
2022-09-01 21:03:54 -0500

    custom_cassandra_class

--- python-cassandra/custom_cassandra_class.py
+++ python-cassandra/custom_cassandra_class.py
@@ -109,9 +109,13 @@ class CustomCassandra:
         # return type of this method is an ordered dictionary
         return row_result_as_ordered_dict
 
-    # Method to execute a cql query
-    # Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+    # 
+    # 
     def exec_select_return_ordered_dict_fetch_all(self, _query):
+        '''
+        Method to execute a cql query
+        Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+        '''
         session = self.db_session()
         # This line sets up the return type of the query as an ordered dictionary
         session.row_factory = ordered_dict_factory
