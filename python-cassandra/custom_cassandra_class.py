db8469ceb5919eb5a374de9cfaf49a9ceb45bbbd
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-03 10:28:46 -0500

    custom_cassandra_class

--- python-cassandra/custom_cassandra_class.py
+++ python-cassandra/custom_cassandra_class.py
@@ -40,10 +40,11 @@ class CustomCassandra:
         return session
     
     # 
-    # Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
+    # 
     def exec_select_return_tuple_fetch_one(self, _query):
         '''
         Method to execute a cql query
+        Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
         '''
         session = self.db_session()
         # This line sets up the return type of the query as a tuple
