ee1352a420e5cc361bba89464cbe10ef91ea97b8
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-03 10:29:47 -0500

    custom_cassandra_class

--- python-cassandra/custom_cassandra_class.py
+++ python-cassandra/custom_cassandra_class.py
@@ -30,8 +30,11 @@ class CustomCassandra:
         self.my_keyspace = _keyspace_name
         return self.my_keyspace
     
-    # Method to connect to Cassandra Cluster on the remote host
+    # 
     def db_session(self):
+        '''
+        Method to connect to Cassandra Cluster on the remote host
+        '''
         # Read the host, port and authorization params from the config file
         # The configurations must match with ~.cassandra/cqlshrc on server
         cluster = Cluster([self.host], port=self.port, auth_provider=self.auth)
