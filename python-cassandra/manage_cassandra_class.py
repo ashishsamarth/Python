98db066c8bcb7530bc97e8387029fbed7123bdbc
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-04 21:04:02 -0500

    manage_cassandra_class

--- python-cassandra/manage_cassandra_class.py
+++ python-cassandra/manage_cassandra_class.py
@@ -30,10 +30,11 @@ class ManageCassandra:
         return remote_connect
     
     # 
-    # Argument to this method is: command provided by end user
+    # 
     def execute_command(self, _command):
         '''
         Method to execute specific commands on the connected host
+        Argument to this method is: command provided by end user
         '''
         stdin, stdout, stderr = self.ssh.exec_command(_command)
         # standard output from the command execution is sent to a variable
