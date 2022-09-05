8cee85bbae7b10b5780e4eb25204c9a2380da1ef
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-04 21:02:47 -0500

    manage_cassandra_class

--- python-cassandra/manage_cassandra_class.py
+++ python-cassandra/manage_cassandra_class.py
@@ -25,7 +25,8 @@ class ManageCassandra:
     # Argument to this method is: Hostname, Username and Location of Private Key for authentication
     def host_connect(self):
         '''
-        Method to remotely connect to host  
+        Method to remotely connect to host
+        Argument to this method is: Hostname, Username and Location of Private Key for authentication
         '''
         remote_connect = self.ssh.connect(hostname=self.hostname, username=self.username, key_filename = self.key_filename)
         return remote_connect
