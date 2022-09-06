03e9b4227a6445aa3d0c11cc421af43a256113d7
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-05 20:27:55 -0500

    manage_cassandra_class

--- python-cassandra/manage_cassandra_class.py
+++ python-cassandra/manage_cassandra_class.py
@@ -6,12 +6,12 @@ import paramiko_conf
 
 # Class definitions should use CamelCase convention based on pep-8 guidelines
 class ManageCassandra:
-    # Initialize the class to set the SSH client and set up host addition policy
+    # 
     # Arguments to this method is - **kwargs
     # **Key word argument (has three params, user, password and the location of private key)
     def __init__(self, **connection_params):
         '''
-        
+        Initialize the class to set the SSH client and set up host addition policy
         '''
         self.ssh = paramiko.SSHClient()
         # setting up the host addition policy is important since, if not defined it will throw paramiko.ssh_exception.SSHException
