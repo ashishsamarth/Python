0249b0bbd0042cb3b3f9ca36588ccd820e4cf014
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-06 12:04:42 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -79,14 +79,19 @@ class CustomCxOracle:
         Arguments to this Method: Sql Query or SQL Variable
         Note: Method verifies the existence of create keyword in the input statement
         '''
-        # Execute the statement, if create keyword is present in the sql query
-        if 'create'.casefold() in str(_sql_query_or_sql_variable).casefold():
-            with self.db_auto_connect.cursor() as cursor:
-                cursor.execute(_sql_query_or_sql_variable)
-            # Once the statement is executed, perform commit of the executed statement
-            self.db_commit()
-        else:
-            print(f'SQL Statement Error: Input SQL does not seem to be a valid Create Statement')
+        try:
+            if 'create'.casefold() in str(_sql_query_or_sql_variable).casefold() and not self.chk_db_object_existence(_sql_query_or_sql_variable):
+                with self.db_auto_connect.cursor() as cursor:
+                    cursor.execute(_sql_query_or_sql_variable)
+                self.db_commit()
+                return f'DB Object: Created Successfully'
+            else:
+                return f'SQL Statement Error; Input SQL does not see to be a valid Create Statement'
+        except cx_Oracle.DatabaseError as ora_error:
+            ecode, emsg = ora_error.args[0].message[:-1].split(': ', 1)
+            if int(ecode.split('-')[-1].lstrip('0')) in CustomCxOracle._oracle_error_map.keys():
+                return CustomCxOracle._oracle_error_map[(int(ecode.split('-')[-1].lstrip('0')))]
+            return 'Method - create_db_object_auto_commit: Unmapped Error Code'
     
     def create_db_object(self, _sql_query_or_sql_variable):
         '''
