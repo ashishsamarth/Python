960c348c60e77cc80d6b87966499ac9af1daee67
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-05 20:32:41 -0500

    exec_select_return_ordered_dict_fetch_one

--- python-cassandra/useful-functions/exec_select_return_ordered_dict_fetch_one.py
+++ python-cassandra/useful-functions/exec_select_return_ordered_dict_fetch_one.py
@@ -3,6 +3,9 @@ from cassandra.query import ordered_dict_factory
 # Method to execute a cql query
 # Note the query must have the keyspace included in it, if 'set_keyspace' method was not executed before this
 def exec_select_return_ordered_dict_fetch_one(self, _query):
+    '''
+    
+    '''
     session = self.db_session()
     # This line sets up the return type of the query as an ordered dictionary
     session.row_factory = ordered_dict_factory
