d24e69111662ff096f9ee320843dd55a11857735
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-08 09:23:16 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -163,9 +163,11 @@ class CustomCxOracle:
         '''
         return self.db_auto_connect.version
 
-    # Method to create a cursor on the connected database
-    # Arguments to this method: None
     def db_cursor_open(self):
+        '''
+        Method to create a cursor on the connected database
+        Arguments to this method: None
+        '''
         print('Cursor is manually opened, always remember to close it by calling db_cursor_close')
         return self.db_auto_connect.cursor()
 
