77f031f2364cb150699990796b1a4e7885bf6969
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-08 09:28:19 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -248,10 +248,12 @@ class CustomCxOracle:
         # return type of this method is set
         return results
 
-    # Method to execute a sql query or a query stored in a variable & fetch all results
-    # Argument to this method is: SQL Query or Variable containing the SQL query
-    # Note: Both Column names & Values are returned as a dictionary
     def db_execute_sql_fetch_all_as_dict(self, _sql_query_or_sql_variable):
+        '''
+        Method to execute a sql query or a query stored in a variable & fetch all results
+        Argument to this method is: SQL Query or Variable containing the SQL query
+        Note: Both Column names & Values are returned as a dictionary
+        '''
         # Open the cursor as 'with' so, it's automatically closed upon task completion
         with self.db_auto_connect.cursor() as cursor:
             # 'arraysize' attribute of cursor is a performance tuning parameter
