0a4c6241b735efb3f8eda08bfa0d508936eacf1c
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-09 21:42:43 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -395,7 +395,6 @@ class CustomCxOracle:
 
     # 
     # 
-    # 
     def db_execute_sql_fetch_top_row_as_tuples_m2(self, _sql_query_or_sql_variable):
         '''
         Method to fetch first row of sql output
