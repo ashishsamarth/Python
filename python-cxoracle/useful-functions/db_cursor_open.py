b58ba668ac1962fc8a979618291ecbb0aaeca081
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-13 21:39:19 -0500

    db_cursor_open

--- python-cxoracle/useful-functions/db_cursor_open.py
+++ python-cxoracle/useful-functions/db_cursor_open.py
@@ -1,5 +1,7 @@
-# Method to create a cursor on the connected database
-# Argument to this method: None
 def db_cursor_open(self):
+    '''
+    Method to create a cursor on the connected database
+    Arguments to this method: None
+    '''
     print('Cursor is manually opened, always remember to close it by calling db_cursor_close')
     return self.db_auto_connect.cursor()
