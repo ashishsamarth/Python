31ed562dc0c9e00d7bc6061a67db9776d5eba8c8
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-13 21:34:08 -0500

    db_get_row_cnt_of_table

--- python-cxoracle/useful-functions/db_get_row_cnt_of_table.py
+++ python-cxoracle/useful-functions/db_get_row_cnt_of_table.py
@@ -1,7 +1,9 @@
-# Method to get the row count in a given table
-# Argument to this method is: Name of the table
-# Argument can be provided as standalone table name or with the schema.table_name
 def db_get_row_cnt_of_table(self, table_name):
+    '''
+    Method to get the row count in a given table
+    Argument to this method is: Name of the table
+    Argument can be provided as standalone table name or with the schema.table_name
+    '''
     # Open the cursor as 'with' so, it's automatically closed upon task completion
     with self.db_auto_connect.cursor() as cursor:
         '''
