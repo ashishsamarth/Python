9aef255792b909ed4cbc1566df956a3d2a2d0a1c
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-13 21:29:21 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -129,20 +129,6 @@ class CustomCxOracle:
         '''
         self.db_auto_connect.commit()
 
-    def db_sys_privileged_conn(self, **privileged_creds: dict):
-        '''
-        Method to create a privileged connection as SYSDBA
-        Arguments to this method: Keyword Argument defined in db_conf as privileged_user
-        '''
-        self.client_version = cx_Oracle.clientversion()
-        # username, password and dsn are read from the db_conf file
-        # mode=cx_Oracle.SYSDBA is the key aspect for this privileged connection
-        # Special note: Do not convert cx_Oracle.SYSDBA to str(cx_Oracle.SYSDBA), since cx_Oracle.SYSDBA results to 2
-        # and cx_Oracle expects the value to be an integer
-        sysdba_conn = cx_Oracle.connect(**privileged_creds)
-        # return type of this method is a cx_Oracle connection object
-        return sysdba_conn
-
     def db_execute_sql_as_sysdba(self, _sql_query_or_sql_variable):
         '''
         Method to execute any sql as sysdba
@@ -1067,4 +1053,18 @@ class CustomCxOracle:
         Method to get the DB version of the connected DB
         Arguments to this method: None
         '''
-        return self.db_auto_connect.version            
+        return self.db_auto_connect.version
+
+    def db_sys_privileged_conn(self, **privileged_creds: dict):
+        '''
+        Method to create a privileged connection as SYSDBA
+        Arguments to this method: Keyword Argument defined in db_conf as privileged_user
+        '''
+        self.client_version = cx_Oracle.clientversion()
+        # username, password and dsn are read from the db_conf file
+        # mode=cx_Oracle.SYSDBA is the key aspect for this privileged connection
+        # Special note: Do not convert cx_Oracle.SYSDBA to str(cx_Oracle.SYSDBA), since cx_Oracle.SYSDBA results to 2
+        # and cx_Oracle expects the value to be an integer
+        sysdba_conn = cx_Oracle.connect(**privileged_creds)
+        # return type of this method is a cx_Oracle connection object
+        return sysdba_conn
