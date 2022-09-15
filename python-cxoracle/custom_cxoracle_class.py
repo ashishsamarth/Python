f89681621870fb668b65f77f5a51c2b067ec1310
ashishsamarth <mrashishsamarth@gmail.com>
2022-09-15 08:21:13 -0500

    custom_cxoracle_class

--- python-cxoracle/custom_cxoracle_class.py
+++ python-cxoracle/custom_cxoracle_class.py
@@ -241,27 +241,6 @@ class CustomCxOracle:
         # return type of this method is dictionary
         return results
 
-    def db_execute_sql_fetch_all_as_tuples(self, _sql_query_or_sql_variable):
-        '''
-        Method to execute a sql query or a query stored in a variable & fetch all results
-        Argument to this method is: SQL Query or Variable containing the SQL query
-        '''
-        # Open the cursor as 'with' so, it's automatically closed upon task completion
-        with self.db_auto_connect.cursor() as cursor:
-            # 'arraysize' attribute of cursor is a performance tuning parameter
-            # For queries returning unknown number of rows (basically large) or production set up
-            # It's advisable to leave the 'prefetchrows' attribute to its default value of 2 and only adjust arraysize
-            # I have kept the arraysize = 500 (same as in sql developer config), but this number should change
-            # based on what kind of production load are we dealing with
-            # Additional Info on Tuning is here:- https://cx-oracle.readthedocs.io/en/latest/user_guide/tuning.html?
-            cursor.arraysize = 500
-            # Execute the SQL Query or Variable containing the SQL query
-            execute = cursor.execute(_sql_query_or_sql_variable)
-            # FetchALL the rows from cursor in to results
-            results = execute.fetchall()
-        # return type of this method is tuple (default behavior)
-        return results
-
     def db_execute_sql_fetch_all_as_list(self, _sql_query_or_sql_variable):
         '''
         Method to execute a sql query or a query stored in a variable & fetch all results
@@ -316,6 +295,27 @@ class CustomCxOracle:
             # FetchALL the rows from cursor in to results
             results = execute.fetchall()
         # return type of this method is set
+        return results
+
+    def db_execute_sql_fetch_all_as_tuples(self, _sql_query_or_sql_variable):
+        '''
+        Method to execute a sql query or a query stored in a variable & fetch all results
+        Argument to this method is: SQL Query or Variable containing the SQL query
+        '''
+        # Open the cursor as 'with' so, it's automatically closed upon task completion
+        with self.db_auto_connect.cursor() as cursor:
+            # 'arraysize' attribute of cursor is a performance tuning parameter
+            # For queries returning unknown number of rows (basically large) or production set up
+            # It's advisable to leave the 'prefetchrows' attribute to its default value of 2 and only adjust arraysize
+            # I have kept the arraysize = 500 (same as in sql developer config), but this number should change
+            # based on what kind of production load are we dealing with
+            # Additional Info on Tuning is here:- https://cx-oracle.readthedocs.io/en/latest/user_guide/tuning.html?
+            cursor.arraysize = 500
+            # Execute the SQL Query or Variable containing the SQL query
+            execute = cursor.execute(_sql_query_or_sql_variable)
+            # FetchALL the rows from cursor in to results
+            results = execute.fetchall()
+        # return type of this method is tuple (default behavior)
         return results
 
     def db_execute_sql_fetch_top_row_as_tuples_m1(self, _sql_query_or_sql_variable):
