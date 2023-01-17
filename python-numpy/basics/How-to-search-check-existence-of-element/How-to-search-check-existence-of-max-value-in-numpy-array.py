fc473d4099c41d612933197e5cb935f583524502
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-17 15:12:32 -0600

    How-to-search-check-existence-of-max-value-in-numpy-array

---
+++ python-numpy/basics/How-to-search-check-existence-of-element/How-to-search-check-existence-of-max-value-in-numpy-array.py
@@ -0,0 +1,10 @@
+# To search / Check existece of largest element in Numpy Array
+# argmax() method is used
+
+import numpy as np
+
+np_var=np.array([1,2,3,4,5,6,9,11,-1,1,3,87,8])
+print('The index of maximum value in numpy array is: ', np.argmax(np_var))
+
+# Output
+# The index of maximum value in numpy array is:  11
