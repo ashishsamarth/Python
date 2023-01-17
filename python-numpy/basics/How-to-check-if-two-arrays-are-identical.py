be8dc6f0b40b1660ef1f579d6d3ecfb8aa0adfe6
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-17 13:05:44 -0600

    How-to-check-if-two-arrays-are-identical

--- python-numpy/basics/How-to-check-if-two-arrays-are-identical.py
+++ python-numpy/basics/How-to-check-if-two-arrays-are-identical.py
@@ -16,3 +16,14 @@ print('Are np_var_1 and np_var_3 identical: ', np.array_equal(np_var_1, np_var_3
 # Are np_var_1 and np_var_2 identical:  True
 # Are np_var_2 and np_var_3 identical:  False
 # Are np_var_1 and np_var_3 identical:  False
+
+# Another Method
+# When compairing two arrays, if we subtract the elements in both arrays based on their index positions, 
+# the output should be zero (if the arrays are identical)
+
+chk_diff_two_arry=(np_var_3 - np_var_2)
+print(np.count_nonzero(chk_diff_two_arry) == 0) #False
+
+# Output
+# False
+
