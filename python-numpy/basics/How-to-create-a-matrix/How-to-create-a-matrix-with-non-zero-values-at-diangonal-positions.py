b0f403eb1c4d971871481151ca392de90fe95d13
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-18 13:59:20 -0600

    How-to-create-a-matrix-with-non-zero-values-at-diangonal-positions

---
+++ python-numpy/basics/How-to-create-a-matrix/How-to-create-a-matrix-with-non-zero-values-at-diangonal-positions.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+np_diag_mat=np.diag((1,2,3,4,5,6))
+print(np_diag_mat)
+
+# Output
+
+'''
+[[1 0 0 0 0 0]
+ [0 2 0 0 0 0]
+ [0 0 3 0 0 0]
+ [0 0 0 4 0 0]
+ [0 0 0 0 5 0]
+ [0 0 0 0 0 6]]
+'''
