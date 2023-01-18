d91c6d31d96bb94604804931ca51da048f1b115e
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-18 13:58:53 -0600

    How-to-create-a-matrix-of-zeros

---
+++ python-numpy/basics/How-to-create-a-matrix/How-to-create-a-matrix-of-zeros.py
@@ -0,0 +1,28 @@
+# To Create a Matrix with specific values
+
+import numpy as np
+
+np_mat=np.zeros((3,3))
+print(np_mat)
+
+# output (The output is float datatype)
+'''
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+'''
+
+# To Create a Matrix with specific values
+
+import numpy as np
+
+np_mat=np.zeros((3,4))
+print(np_mat)
+
+# Output  (The output is float datatype)
+'''
+[[0. 0. 0. 0.]
+ [0. 0. 0. 0.]
+ [0. 0. 0. 0.]]
+'''
+
