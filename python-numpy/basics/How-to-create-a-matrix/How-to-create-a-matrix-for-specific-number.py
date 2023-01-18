050d87786bf68a8c8220a1356276c0252bdbe036
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-18 13:58:18 -0600

    How-to-create-a-matrix-for-specific-number

---
+++ python-numpy/basics/How-to-create-a-matrix/How-to-create-a-matrix-for-specific-number.py
@@ -0,0 +1,43 @@
+# Fill Values in a matrix
+
+import numpy as np
+
+col=6
+
+# How to understand the following line
+# It creates the first row of the array as [0 1 2 3 4 5], since the array range end is 6
+# It creates the second row of the array as [6 7 8 9 10 11], since the start range is 6 and end range is 12
+# It creates the third rows of the array as [12 13 14 15 16 17], since the start range is 12 and end range is 18
+# It creates the fourth rows of the array as [18 19 20 21 22 23], since the start range is 18 and end range is 23
+np_mat_x=np.array([np.arange(col),  np.arange(col, col*2), np.arange(col*2, col*3), np.arange(col*3, col*4)])
+
+print('np_mat_x :  ', np_mat_x)
+print()
+np_mat_transposed = np_mat_x.transpose()
+print('np_mat_transposed:  ', np_mat_transposed)
+print()
+np_mat_transposed.fill(925)
+print('np_mat_transposed:  ', np_mat_transposed)
+
+# Output
+
+'''
+np_mat_x :   [[ 0  1  2  3  4  5]
+ [ 6  7  8  9 10 11]
+ [12 13 14 15 16 17]
+ [18 19 20 21 22 23]]
+
+np_mat_transposed:   [[ 0  6 12 18]
+ [ 1  7 13 19]
+ [ 2  8 14 20]
+ [ 3  9 15 21]
+ [ 4 10 16 22]
+ [ 5 11 17 23]]
+
+np_mat_transposed:   [[925 925 925 925]
+ [925 925 925 925]
+ [925 925 925 925]
+ [925 925 925 925]
+ [925 925 925 925]
+ [925 925 925 925]]
+'''
