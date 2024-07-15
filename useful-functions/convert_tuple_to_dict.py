d709c1ab7b2587da66f0843779b5e73aa04b445e
Ashish Samarth <mrashishsamarth@outlook.com>
2024-07-15 11:31:13 -0500

    convert_tuple_to_dict

--- useful-functions/convert_tuple_to_dict.py
+++ useful-functions/convert_tuple_to_dict.py
@@ -4,7 +4,7 @@
 def convert_tuple_to_dict(inp_tuple, starting_idx_position):
     # define an empty dictionary
     output_dict = dict()
-    # Enumerate over the input tuple with a starting counter as zero
+    # Enumerate over the input set with a starting counter as provided by user
     for count, item in enumerate(inp_tuple,start=starting_idx_position):
         # Assign the counter as key and tuple members as values
         output_dict[count] = item
