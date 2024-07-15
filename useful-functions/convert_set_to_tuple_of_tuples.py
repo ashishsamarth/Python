e8b8ccd360393924c86afe169fa12105765a1c19
Ashish Samarth <mrashishsamarth@outlook.com>
2024-07-15 11:53:17 -0500

    convert_set_to_tuple_of_tuples

---
+++ useful-functions/convert_set_to_tuple_of_tuples.py
@@ -0,0 +1,13 @@
+# Method to convert a set to a tuple of tuples
+# Input:1 - Iterable Sequence
+# Input:2 - Starting Counter position
+# Input:3 - Increment the Counter by this number
+def convert_set_to_tuple_of_tuples(inp_set, starting_idx_position, increment_by):
+    # Validate if the "increment by" is an integer
+    assert type(increment_by) == int, "Increment By MUST be an integer"
+    # Enumerate over the input set with a starting counter as provided by user
+    for count, item in enumerate(inp_set, start=starting_idx_position):
+        # Create a generator object using yield to create the tuple members
+        yield starting_idx_position, item
+        # increment counter position by this number
+        starting_idx_position += increment_by
