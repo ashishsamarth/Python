f405ffa8132042481bcc5576423924e8e122def1
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 18:54:21 -0500

    Update generate_rdm_pass.py
    generate_rdm_pass.py

--- useful-functions/generate_rdm_pass.py
+++ useful-functions/generate_rdm_pass.py
@@ -1,10 +1,13 @@
-# Method to generate a random password
 # Import the string package
 import string
 # From the random module import the choice method
 from random import choice
 # Set the default length of password as 20, if its not provided while calling this method
 def generate_rdm_pass(length=20):
-    # randomly select 20 characters from a combination of letters, digits and punctuations
+    '''
+    Method to generate a random password
+    randomly select 20 characters from a combination of letters, digits and punctuations
+    '''
     return ''.join(choice(string.ascii_letters + string.digits + string.punctuation) for i in range (length))
+
     
