cca24d64b708389a33ab57b7ebb230fedf02f520
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 18:52:49 -0500

    Update get_current_date.py
    get_current_date.py

--- useful-functions/get_current_date.py
+++ useful-functions/get_current_date.py
@@ -1,6 +1,8 @@
 # Import the datetime module
 from datetime import datetime
-# Method to get the current date
+'''
+Method to get the current date
+'''
 def get_current_date():
     fmt = "%m/%d/%Y"
     return datetime.today().strftime(format=fmt)
