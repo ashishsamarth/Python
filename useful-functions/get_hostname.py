5735c65264424c6bbeeae81ed93ced2c54ebba7f
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 18:50:03 -0500

    Update get_hostname.py
    get_hostname.py

--- useful-functions/get_hostname.py
+++ useful-functions/get_hostname.py
@@ -1,9 +1,12 @@
 # Import the platform package
 import platform
 
-# Method to get the hostname
 def get_hostname():
-    # The 'node' method of platform module provides the hostname of the machine
+    '''
+    Method to get the hostname
+    The 'node' method of platform module provides the hostname of the machine
+    '''
     return platform.node()
+    
     # Another way is to use the uname results named tuple
     # return platform.uname().node
