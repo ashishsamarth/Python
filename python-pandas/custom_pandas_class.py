bd202e4d3949e0385fa99c3827e6576295398b4e
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 18:43:55 -0500

    Update custom_pandas_class.py
    Update custom_pandas_class.py

--- python-pandas/custom_pandas_class.py
+++ python-pandas/custom_pandas_class.py
@@ -476,6 +476,7 @@ class CustomPandas:
     def value_frequency_in_col_by_name(self, _col_name: str):
         '''
         Method to get frequency of occurrences per unique item in a given column
-        Argument to this method is: - Column index position
+        Argument to this method is: - Column name
         '''
         return self.my_df[_col_name].value_counts()
+
