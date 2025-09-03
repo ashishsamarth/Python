c918630362149cc79447ba59c8da65883aa54641
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 18:51:34 -0500

    Update get_file_extension.py
    get_file_extension.py

--- useful-functions/get_file_extension.py
+++ useful-functions/get_file_extension.py
@@ -1,7 +1,10 @@
-# Method to get the file extension
-# Argument to the method: filename
 def get_file_extension(self, _filename):
-    # Using the split method of class <str>
-    # split the filename using '.' and capturing the last element of the return type class <list>
+    '''
+    Method to get the file extension
+    Argument to the method: filename
+    
+    Using the split method of class <str>
+    split the filename using '.' and capturing the last element of the return type class <list>
+    '''
     self.file_ext = _filename.split('.')[-1]
     return self.file_ext
