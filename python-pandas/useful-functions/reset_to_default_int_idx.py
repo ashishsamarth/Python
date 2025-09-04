3107ee6928aabc8c44506ad176edb2b20110b66c
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:33:58 -0500

    Update reset_to_default_int_idx.py
    reset_to_default_int_idx.py

--- python-pandas/useful-functions/reset_to_default_int_idx.py
+++ python-pandas/useful-functions/reset_to_default_int_idx.py
@@ -1,4 +1,6 @@
-# Method to reset the index to default integer values
-# No Arguments to this method
 def reset_to_default_int_idx(self):
+    '''
+    Method to reset the index to default integer values
+    No Arguments to this method
+    '''
     return self.my_df.reset_index()
