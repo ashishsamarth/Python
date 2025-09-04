92cd354911d574a6595a6dd5deba9195853bef6f
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:48:44 -0500

    Update drop_col_by_index.py
    drop_col_by_index.py

--- python-pandas/useful-functions/drop_col_by_index.py
+++ python-pandas/useful-functions/drop_col_by_index.py
@@ -1,8 +1,11 @@
-# Method to remove a column using its existing position
-# Note: This method is build using a try: except block to deal with index out of bounds exception
 def drop_col_by_index(self, _col_idx):
+    '''
+    Method to remove a column using its existing position
+    Note: This method is build using a try: except block to deal with index out of bounds exception    
+    '''
     try:
         self.my_df.drop(self.my_df.columns[_col_idx], axis=1, inplace=True)
     except IndexError:
         print(
             f'ERROR: - Index value {_col_idx} is out of bounds, Current Index length is {self.my_df.shape[-1]}')
+
