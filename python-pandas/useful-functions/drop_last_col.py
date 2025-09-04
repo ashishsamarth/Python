492c1a9ca352d3eb4510910f22b95c5454d33ab2
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:47:35 -0500

    Update drop_last_col.py
    drop_last_col.py

--- python-pandas/useful-functions/drop_last_col.py
+++ python-pandas/useful-functions/drop_last_col.py
@@ -1,6 +1,9 @@
-# Method to drop the last columns in the dataframe
-# Note: This method assumes that there's at least 1 column in the dataframe
 def drop_last_col(self):
+    '''
+    Method to drop the last columns in the dataframe
+    Note: This method assumes that there's at least 1 column in the dataframe    
+    '''
     if len(self.my_df.columns.values) > 0:
         self.my_df.drop(self.my_df.columns[-1], axis=1, inplace=True)
+
     return 'dataframe is empty'
