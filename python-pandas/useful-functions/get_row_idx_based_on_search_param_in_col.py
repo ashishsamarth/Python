630c3c5c3a67e0352aebd702fadffcbcb7181cc7
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:43:35 -0500

    Update get_row_idx_based_on_search_param_in_col.py
    get_row_idx_based_on_search_param_in_col.py

--- python-pandas/useful-functions/get_row_idx_based_on_search_param_in_col.py
+++ python-pandas/useful-functions/get_row_idx_based_on_search_param_in_col.py
@@ -1,6 +1,9 @@
-# Method to get row values based on a search param in a given column name
-# Arguments to this method are: - Column name to be searched and search value
 def get_row_idx_based_on_search_param_in_col(self, _col_name, _search_val):
+    '''
+    Method to get row values based on a search param in a given column name
+    Arguments to this method are: - Column name to be searched and search value    
+    '''
     _col_idx = self.get_col_idx_by_name(_col_name)
     # Nested list comprehension returns the pandas index from _val[0]
+
     return [_val[0] for _val in [_ for _ in self.my_df.itertuples() if _[_col_idx + 1] == _search_val]]
