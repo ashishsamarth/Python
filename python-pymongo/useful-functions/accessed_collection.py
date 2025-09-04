2b55f12463c1a8cd7c6993ac5e0113a6b3cbccc2
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:00:45 -0500

    Update accessed_collection.py
    accessed_collection.py

--- python-pymongo/useful-functions/accessed_collection.py
+++ python-pymongo/useful-functions/accessed_collection.py
@@ -1,8 +1,11 @@
-# Method to access a collection in connected DB of Mongo Instance
-# Argument to this method: A valid collection name
 def accessed_collection(self, _collection_name):
+    '''
+    Method to access a collection in connected DB of Mongo Instance
+    Argument to this method: A valid collection name
+    '''
     note = 'Collection Name provided is not a present in Connected Database'
     assert _collection_name in self.get_collection_names(), note
     # If case of Assertion error, the collection cannot be accessed
     _my_collection = self.connected_db[_collection_name]
+
     return _my_collection
