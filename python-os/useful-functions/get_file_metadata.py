73d49ab4c2792a6696f127ad3b98ad7df56ee9e9
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 20:14:04 -0500

    Update get_file_metadata.py
    get_file_metadata.py

--- python-os/useful-functions/get_file_metadata.py
+++ python-os/useful-functions/get_file_metadata.py
@@ -1,6 +1,8 @@
-# Method to get file metadata
-# Argument to this method: input filename
 def get_file_metadata(self):
+    '''
+    Method to get file metadata
+    Argument to this method: input filename    
+    '''
     file_metadata = {'File_Name': self.my_filename,
                  'File_Owner_username': getpwuid(os.stat(self.my_filename).st_uid).pw_name,
                  'File_Owner_name': getpwuid(os.stat(self.my_filename).st_uid).pw_gecos,
@@ -8,4 +10,5 @@ def get_file_metadata(self):
                  'Accessed': ctime(os.stat(self.my_filename).st_atime),
                  'Modified': ctime(os.stat(self.my_filename).st_mtime)
                  }
+
     return file_metadata
