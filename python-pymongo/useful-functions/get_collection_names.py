e1a203759569ac106a896e83abd29a19b305790d
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:04:25 -0500

    Update get_collection_names.py
    get_collection_names.py

--- python-pymongo/useful-functions/get_collection_names.py
+++ python-pymongo/useful-functions/get_collection_names.py
@@ -1,3 +1,5 @@
-# Method to get the collection names for a connected DB of Mongo Instance
 def get_collection_names(self):
+    '''
+    Method to get the collection names for a connected DB of Mongo Instance
+    '''
     return self.connected_db.list_collection_names()
