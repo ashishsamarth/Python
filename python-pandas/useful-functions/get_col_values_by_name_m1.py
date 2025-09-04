06261b46797cc7efcb247b21dab630f28c4905c3
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:44:40 -0500

    Update get_col_values_by_name_m1.py
    get_col_values_by_name_m1.py

--- python-pandas/useful-functions/get_col_values_by_name_m1.py
+++ python-pandas/useful-functions/get_col_values_by_name_m1.py
@@ -1,5 +1,7 @@
-# Method to get the values from a given column
-# Argument to this method is: - Column name
 def get_col_values_by_name_m1(self, _col_name):
+    '''
+    Method to get the values from a given column
+    Argument to this method is: - Column name
+    '''
     # Return type is dependent on column values
     return self.my_df[_col_name]
