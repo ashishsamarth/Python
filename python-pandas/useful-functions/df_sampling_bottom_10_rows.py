0728d5c2e58637b191a77eb6b993123f5f62df47
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:52:23 -0500

    Update df_sampling_bottom_10_rows.py
    df_sampling_bottom_10_rows.py

--- python-pandas/useful-functions/df_sampling_bottom_10_rows.py
+++ python-pandas/useful-functions/df_sampling_bottom_10_rows.py
@@ -1,4 +1,6 @@
-# Method to see df sample - Bottom 10 rows
-# No Arguments for this method
 def df_sampling_bottom_10_rows(self):
+    '''
+    Method to see df sample - Bottom 10 rows
+    No Arguments for this method
+    '''
     return self.my_df.tail(10)
