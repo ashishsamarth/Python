8344ceae0a0d5dfabd8df8ede014a14b4aba138a
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:11:35 -0500

    Update custom_paramiko_class.py
    custom_paramiko_class.py

--- python-paramiko/custom_paramiko_class.py
+++ python-paramiko/custom_paramiko_class.py
@@ -3,14 +3,15 @@ import paramiko
 # Import host_conf : This holds the connection params
 import host_conf
 
-
 # Class definitions should use CamelCase convention based on pep-8 guidelines
 class CustomParamiko:
 
-    # Initialize the class to set the SSH client and set up host addition policy
-    # Arguments to this method is - **kwargs
-    # **Key word argument (has three params, user, password and the location of private key)
     def __init__(self, **connection_params: dict):
+        '''
+        Initialize the class to set the SSH client and set up host addition policy
+        Arguments to this method is - **kwargs
+        **Key word argument (has three params, user, password and the location of private key)
+        '''
         self.ssh = paramiko.SSHClient()
         # setting up the host addition policy is important since, if not defined it will throw
         # paramiko.ssh_exception.SSHException
@@ -24,20 +25,26 @@ class CustomParamiko:
         # I have used the absolute path for the key location, one can always use os.path() to manage this
         self.key_filename = connection_params.get('key_filename')
 
-    # Method to remotely connect to host
-    # Argument to this method is: Hostname, Username and Location of Private Key for authentication
     def host_connect(self):
+        '''
+        Method to remotely connect to host
+        Argument to this method is: Hostname, Username and Location of Private Key for authentication
+        '''
         return self.ssh.connect(hostname=self.hostname, username=self.username, key_filename = self.key_filename)
 
-    # Method to execute specific commands on the connected host
-    # Argument to this method is: command provided by end user
-    # Note: read through 'https://stackoverflow.com/questions/42897381' for directory navigation
-    # paramiko is only good if you run chained commands like 'cd some_path; ls -lrt', non-chained commands do not work very well
     def execute_command(self, _command):
+        '''
+        Method to execute specific commands on the connected host
+        Argument to this method is: command provided by end user
+        Note: read through 'https://stackoverflow.com/questions/42897381' for directory navigation
+        paramiko is only good if you run chained commands like 'cd some_path; ls -lrt', non-chained commands do not work very well
+        '''
         self.ssh.exec_command(_command)
         return 'command executed successfully'
 
-    # Method to close the connection of remote host
-    # Argument to this method is: None
     def close_connection(self):
+        '''
+        Method to close the connection of remote host  
+        Argument to this method is: None
+        '''
         return self.ssh.close()
