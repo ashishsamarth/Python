29b648310d4b4b139c352054e7350b5252c1e961
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:45:50 -0500

    Update get_col_unique_values_by_name.py
    get_col_unique_values_by_name.py

--- python-pandas/useful-functions/get_col_unique_values_by_name.py
+++ python-pandas/useful-functions/get_col_unique_values_by_name.py
@@ -1,5 +1,8 @@
-# Method to get the unique values from a given column
-# Argument to this method is: - Column name
 def get_col_unique_values_by_name(self, _col_name):
+    '''
+    Method to get the unique values from a given column
+    Argument to this method is: - Column name
+    '''
     # Return type is dependent on column values
+
     return self.my_df[_col_name].unique()
