5f292ab48bb5e90698323e13b439c2f7998c2652
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:33:28 -0500

    Update save_df_diff_tgt.py
    save_df_diff_tgt.py

--- python-pandas/useful-functions/save_df_diff_tgt.py
+++ python-pandas/useful-functions/save_df_diff_tgt.py
@@ -1,8 +1,11 @@
-# Method to save the dataframe to a different file
-# Based on the file extension appropriate pandas function will be called to save the data
 def save_df_diff_tgt(self, _tgt_file_name):
+    '''
+    Method to save the dataframe to a different file
+    Based on the file extension appropriate pandas function will be called to save the data
+    '''
     # If the file extension is xlsx then 'to_excel' will be called
     if self.my_filename.split('.')[-1] == 'xlsx':
         return self.my_df.to_excel(_tgt_file_name, index=False)
     elif self.my_filename.split('.')[-1] == 'csv':
+
         return self.my_df.to_csv(_tgt_file_name, index=False)
