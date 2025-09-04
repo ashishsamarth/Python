7ba45c118578a9f2bb728720f6315efd4712e272
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:45:09 -0500

    Update get_col_values_by_idx.py
    get_col_values_by_idx.py

--- python-pandas/useful-functions/get_col_values_by_idx.py
+++ python-pandas/useful-functions/get_col_values_by_idx.py
@@ -1,4 +1,6 @@
-# Method to get the values from a given column index position
-# Return type is dependent on column values
 def get_col_values_by_idx(self, _col_idx):
+    '''
+    Method to get the values from a given column index position
+    Return type is dependent on column values
+    '''
     return self.my_df[self.my_df_col_idx_name[_col_idx]]
