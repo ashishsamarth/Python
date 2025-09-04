f15d42711ed9b1754cdd9a39139fcc061ca848c4
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:13:58 -0500

    Update host_connect.py
    host_connect.py

--- python-paramiko/useful-functions/host_connect.py
+++ python-paramiko/useful-functions/host_connect.py
@@ -1,4 +1,6 @@
-# Method to remotely connect to host
-# Argument to this method is: Hostname, Username and Location of Private Key for authentication
 def host_connect(self):
+    '''
+    Method to remotely connect to host
+    Argument to this method is: Hostname, Username and Location of Private Key for authentication
+    '''
     return self.ssh.connect(hostname=self.hostname, username=self.username, key_filename = self.key_filename)
