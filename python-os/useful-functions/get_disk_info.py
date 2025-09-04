9045d6a65007af443250d9b597c1e62c5fa4a22c
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 20:14:40 -0500

    Update get_disk_info.py
    get_disk_info.py

--- python-os/useful-functions/get_disk_info.py
+++ python-os/useful-functions/get_disk_info.py
@@ -1,5 +1,7 @@
-# Method to get disk information
 def get_disk_info():
+    '''
+    Method to get disk information
+    '''
     # Capture the disk usage in three different variable (Note these are in bytes)
     total_b, used_b, available_b = shutil.disk_usage('.')
     gb = 10 ** 9
@@ -11,4 +13,5 @@ def get_disk_info():
     disk_info={'Total': total_gb,
            'Used': used_gb,
            'Available': available_gb}
+
     return disk_info
