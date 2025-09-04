51291bea1d28038dc265320b2d139feb9f3c813a
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:32:15 -0500

    Update set_col_as_index.py
    set_col_as_index.py

--- python-pandas/useful-functions/set_col_as_index.py
+++ python-pandas/useful-functions/set_col_as_index.py
@@ -1,4 +1,6 @@
-# Method to set a column as the index column
-# Argument to this method is: - Column name which needs to be set as index
 def set_col_as_index(self, _col_name):
+    '''
+    Method to set a column as the index column
+    Argument to this method is: - Column name which needs to be set as index    
+    '''
     return self.my_df.set_index(_col_name)
