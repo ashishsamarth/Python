6733b22b40a7e7808857c04a3db5db5ff3fbcf8e
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:35:21 -0500

    Update rename_col.py
    rename_col.py

--- python-pandas/useful-functions/rename_col.py
+++ python-pandas/useful-functions/rename_col.py
@@ -1,9 +1,12 @@
-# Method to rename an existing column
-# Arguments to this method are: - Old column name and New Column Name
 def rename_col(self, _old_col_name, _new_col_name):
+    '''
+    Method to rename an existing column
+    Arguments to this method are: - Old column name and New Column Name
+    '''
     try:
         # Assign the renamed columns to original dataframe
         # so that changes are reflected in spreadsheet
         self.my_df = self.my_df.rename(columns={_old_col_name: _new_col_name})
     except KeyError:
+
         print(f' Column Name :- "{_old_col_name}" not found in the dataframe')
