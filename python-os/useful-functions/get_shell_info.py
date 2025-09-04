b14c45db47aefd80bacade4df4b6f3b54c2e45c8
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 20:13:02 -0500

    Update get_shell_info.py
    get_shell_info.py

--- python-os/useful-functions/get_shell_info.py
+++ python-os/useful-functions/get_shell_info.py
@@ -1,6 +1,9 @@
-# Method to get the shell
-# Assumption: OS is Linux
 def get_shell_info(self):
+    '''
+    Method to get the shell
+    Assumption: OS is Linux
+    '''
     if self.os_type == 'Linux':
         return os.environ['SHELL'].split('/')[-1]
+
         
