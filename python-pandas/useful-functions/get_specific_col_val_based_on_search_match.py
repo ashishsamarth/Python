eb195ccbebb3ab0d932cc58a4900f4662f36672f
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:41:22 -0500

    Update get_specific_col_val_based_on_search_match.py
    get_specific_col_val_based_on_search_match.py

--- python-pandas/useful-functions/get_specific_col_val_based_on_search_match.py
+++ python-pandas/useful-functions/get_specific_col_val_based_on_search_match.py
@@ -1,8 +1,11 @@
-# Method to get values of  specific columns if search value matched in another column
-# Arguments to this method are: - Column Name to the searched,  Value to be searched, Values to be seen
 def get_specific_col_val_based_on_search_match(self, _search_col_name, _search_val, _see_val_col_name):
+    '''
+    Method to get values of  specific columns if search value matched in another column
+    Arguments to this method are: - Column Name to the searched,  Value to be searched, Values to be seen
+    '''
     # get the column indexes
     _search_col_idx = self.get_col_idx_by_name(_search_col_name)
     _see_val_col_idx = self.get_col_idx_by_name(_see_val_col_name)
     return [_val[_see_val_col_idx + 1] for _val in
             [_ for _ in self.my_df.itertuples() if _[_search_col_idx + 1] == _search_val]]
+
