d0d205321d0f108fbcbe41cc4690cf28bcaf10eb
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:12:23 -0500

    Update __init__class.py
    __init__class.py

--- python-paramiko/useful-functions/__init__class.py
+++ python-paramiko/useful-functions/__init__class.py
@@ -7,10 +7,12 @@ import host_conf
 # Class definitions should use CamelCase convention based on pep-8 guidelines
 class CustomParamiko:
 
-    # Initialize the class to set the SSH client and set up host addition policy
-    # Arguments to this method is - **kwargs
-    # **Key word argument (has three params, user, password and the location of private key)
     def __init__(self, **connection_params):
+        '''
+        Initialize the class to set the SSH client and set up host addition policy
+        Arguments to this method is - **kwargs
+        **Key word argument (has three params, user, password and the location of private key)       
+        '''
         self.ssh = paramiko.SSHClient()
         # setting up the host addition policy is important since, if not defined it will throw
         # paramiko.ssh_exception.SSHException
@@ -21,4 +23,5 @@ class CustomParamiko:
         # assign the user provided db_user to initialization method, for reuse across all methods
         self.username = connection_params.get('username')
         # assign the user provided db_user to initialization method, for reuse during connection
+
         self.key_filename = connection_params.get('key_filename')
