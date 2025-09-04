54cc1e5cf87ed477cba62bffa297a704f98b27b5
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:46:58 -0500

    Update get_col_idx_by_name.py
    get_col_idx_by_name.py

--- python-pandas/useful-functions/get_col_idx_by_name.py
+++ python-pandas/useful-functions/get_col_idx_by_name.py
@@ -1,6 +1,8 @@
-# Method to get the column index based on column name
-# Argument to this method is: - Column name
 def get_col_idx_by_name(self, _col_name):
+    '''
+    Method to get the column index based on column name
+    Argument to this method is: - Column name
+    '''
     # return type is integer
     # get_loc will get the index of the column
     return self.my_df.columns.get_loc(_col_name)
