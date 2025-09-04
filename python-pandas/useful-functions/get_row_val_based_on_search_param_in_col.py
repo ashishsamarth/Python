dcec3447f933074b173360fc32f1ad1938317f5a
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:43:06 -0500

    Update get_row_val_based_on_search_param_in_col.py
    get_row_val_based_on_search_param_in_col.py

--- python-pandas/useful-functions/get_row_val_based_on_search_param_in_col.py
+++ python-pandas/useful-functions/get_row_val_based_on_search_param_in_col.py
@@ -1,6 +1,9 @@
-# Method to get row values based on a search param in a given column name
-# Arguments to this method are: - Column name to be searched and search value
 def get_row_val_based_on_search_param_in_col(self, _col_name, _search_val):
+    '''
+    Method to get row values based on a search param in a given column name
+    Arguments to this method are: - Column name to be searched and search value
+    '''
     _col_idx = self.get_col_idx_by_name(_col_name)
     # Return the entire row, if searched value was found in the given column
+
     return [_ for _ in self.my_df.itertuples() if _[_col_idx + 1] == _search_val]
