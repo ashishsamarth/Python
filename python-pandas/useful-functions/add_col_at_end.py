e547ecec92c3ea897606645244474224850de45e
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:57:52 -0500

    Update add_col_at_end.py
    add_col_at_end.py

--- python-pandas/useful-functions/add_col_at_end.py
+++ python-pandas/useful-functions/add_col_at_end.py
@@ -1,6 +1,8 @@
-# Method to add a new column at the end of the dataframe
-# Input Argument is a Column name of user choice
 def add_col_at_end(self, _col_name):
+    '''
+    Method to add a new column at the end of the dataframe
+    Input Argument is a Column name of user choice
+    '''
     # The following statement checks if the user provided column name already exists in the dataframe or not
     # The new column will only be added if it does not exist already
     if _col_name not in self.my_df.columns:
@@ -9,3 +11,4 @@ def add_col_at_end(self, _col_name):
     # Else the new column name will not be added to the dataframe
     else:
         print(f'Column :- "{_col_name}" is already present in the Dataframe, skipping addition...', end='\n')
+
