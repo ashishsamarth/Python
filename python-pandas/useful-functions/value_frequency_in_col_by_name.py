59f6c16e340925858aba561aad4beb57e80f2206
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:30:31 -0500

    Update value_frequency_in_col_by_name.py
    value_frequency_in_col_by_name.py

--- python-pandas/useful-functions/value_frequency_in_col_by_name.py
+++ python-pandas/useful-functions/value_frequency_in_col_by_name.py
@@ -1,4 +1,6 @@
-# Method to get frequency of occurrences per unique item in a given column
-# Argument to this method is: - Column index position
 def value_frequency_in_col_by_name(self, _col_name):
+    '''
+    Method to get frequency of occurrences per unique item in a given column
+    Argument to this method is: - Column index position
+    '''
     return self.my_df[_col_name].value_counts()
