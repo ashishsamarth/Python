27a2b64ba0146bc97e3b3d4d5decf88496ff85df
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:13:30 -0500

    Update execute_command.py
    execute_command.py

--- python-paramiko/useful-functions/execute_command.py
+++ python-paramiko/useful-functions/execute_command.py
@@ -1,6 +1,9 @@
-# Method to execute specific commands on the connected host
-# Argument to this method is: command provided by end user
 def execute_command(self, _command):
+    '''
+    Method to execute specific commands on the connected host
+    Argument to this method is: command provided by end user
+    '''
     stdin, stdout, stderr = self.ssh.exec_command(_command)
     command_output = stdout.read()
+
     return command_output
