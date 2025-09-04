d8f724644bffe6bd71b5655a2385a1467f82ecd0
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:54:13 -0500

    Update create_df_from_filtered_data_multiple_or_conditions.py
    create_df_from_filtered_data_multiple_or_conditions.py

--- python-pandas/useful-functions/create_df_from_filtered_data_multiple_or_conditions.py
+++ python-pandas/useful-functions/create_df_from_filtered_data_multiple_or_conditions.py
@@ -1,10 +1,13 @@
 import pandas as pd
-# Method to create dataframe with based on multiple or conditions
-# argument: _col_names should preferably be a list
-# argument: _conditions should preferably be a list
-# It has to be a 1:1 mapping of _col_name and condition and is sequence sensitive
-# Do not convert is to a static method
+
 def create_df_from_filtered_data_multiple_or_conditions(self, _col_names, _conditions):
+    '''
+    Method to create dataframe with based on multiple or conditions
+    argument: _col_names should preferably be a list
+    argument: _conditions should preferably be a list
+    It has to be a 1:1 mapping of _col_name and condition and is sequence sensitive
+    Do not convert is to a static method
+    '''
     try:
         if len(_col_names) == len(_conditions):
             _filter_con = []
@@ -14,4 +17,5 @@ def create_df_from_filtered_data_multiple_or_conditions(self, _col_names, _condi
             _filtered_data_set = eval(delta)
             return pd.DataFrame(_filtered_data_set)
     except:
+
         print("Unexpected-Condition-In-Method")
