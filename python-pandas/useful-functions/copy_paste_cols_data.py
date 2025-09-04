b58cb5549793c1034e2606e4facd7a8929ce125e
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:56:23 -0500

    Update copy_paste_cols_data.py
    copy_paste_cols_data.py

--- python-pandas/useful-functions/copy_paste_cols_data.py
+++ python-pandas/useful-functions/copy_paste_cols_data.py
@@ -1,8 +1,11 @@
-# Method to copy the contents from one column to another column of dataframe
-# Arguments to this method are source column name and target column name
 def copy_paste_cols_data(self, _src_col_name, _tgt_col_name):
+    '''
+    Method to copy the contents from one column to another column of dataframe
+    Arguments to this method are source column name and target column name
     # NOTE: - If target col does not exist in the dataframe, it will be created at run time
+    '''
     try:
         self.my_df[_tgt_col_name] = self.my_df[_src_col_name]
     except KeyError:
+
         print(f' Source column :- "{_src_col_name}"  not found in worksheet')
