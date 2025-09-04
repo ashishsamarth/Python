38ad7bba28944be89f922d93ffafc683679bb12d
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:03:24 -0500

    Update connect_db.py
    connect_db.py

--- python-pymongo/useful-functions/connect_db.py
+++ python-pymongo/useful-functions/connect_db.py
@@ -1,6 +1,8 @@
-# Method to connect (use) specific Mongo-DB or Schema
-# Argument to this method is:- Database Name
 def connect_db(self, _db_name):
+    '''
+    Method to connect (use) specific Mongo-DB or Schema
+    Argument to this method is:- Database Name    
+    '''
     note = 'Database Name provided, does not exist in connected Mongo Instance'
     assert _db_name in self.show_dbs(), note
     # following line connects to the user provided DB, only if its a existing DB in mongo instance
@@ -8,4 +10,5 @@ def connect_db(self, _db_name):
     # Set the value of following property in __init__() with the user provided database name
     self.connected_db = _my_db
     # return type of this method is the DB instance
+
     return _my_db
