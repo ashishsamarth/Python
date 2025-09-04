cbc2f4f05c628f48cc373c87ebb59bbec84c813d
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:31:40 -0500

    Update suffix_delim.py
    suffix_delim.py

--- python-pandas/useful-functions/suffix_delim.py
+++ python-pandas/useful-functions/suffix_delim.py
@@ -1,4 +1,6 @@
-# Method to add a delimiter as suffix
-# Note:- This was added for understanding, however their are pre-built functions in pandas for this task
 def suffix_delim(self, _tgt_col_name, _delim):
+    '''
+    Method to add a delimiter as suffix
+    Note:- This was added for understanding, however their are pre-built functions in pandas for this task
+    '''
     self.my_df[_tgt_col_name] = self.my_df[_tgt_col_name].astype(str) + _delim
