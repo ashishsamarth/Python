603825cd00e383fe8fe7353e9de1294825609114
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:04:55 -0500

    Update show_dbs.py
    show_dbs.py

--- python-pymongo/useful-functions/show_dbs.py
+++ python-pymongo/useful-functions/show_dbs.py
@@ -1,3 +1,5 @@
-# Method to see existing database in connected Mongo Instance
 def show_dbs(self):
+    '''
+    Method to see existing database in connected Mongo Instance
+    '''
     return self.my_client.list_database_names()
