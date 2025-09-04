1249ea2950ab827866f1c073ec83c2503a09e0e0
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:46:26 -0500

    Update get_col_unique_values_by_idx.py
    get_col_unique_values_by_idx.py

--- python-pandas/useful-functions/get_col_unique_values_by_idx.py
+++ python-pandas/useful-functions/get_col_unique_values_by_idx.py
@@ -1,5 +1,7 @@
-# Method to get the unique values from a given column index position
-# Argument to this method is: - Column Index
 def get_col_unique_values_by_idx(self, _col_idx):
+    '''
+    Method to get the unique values from a given column index position
+    Argument to this method is: - Column Index
+    '''
     # Return type is dependent on column values
     return self.my_df[self.my_df_col_idx_name[_col_idx]].unique()
