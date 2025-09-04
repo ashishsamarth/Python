8eaf603a05d0d76c1af899b4e99c22e772127b7f
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:55:48 -0500

    Update create_df_from_dataset.py
    create_df_from_dataset.py

--- python-pandas/useful-functions/create_df_from_dataset.py
+++ python-pandas/useful-functions/create_df_from_dataset.py
@@ -1,8 +1,12 @@
 import pandas as pd
-# Method to create a dataframe from a dataset
-# Argument to this method is: - Dataset
+
 def create_df_from_dataset(self, _dataset):
+    '''
+    Method to create a dataframe from a dataset
+    Argument to this method is: - Dataset
+    '''
     # create a dataframe using the user provided dataset
     self.my_df = pd.DataFrame(_dataset)
     # return Dataframe
+
     return self.my_df
