27141da34ac5f3bdb52b415f7f52ba1d7cedfd46
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 19:38:27 -0500

    Update prefix_delim.py
    prefix_delim.py

--- python-pandas/useful-functions/prefix_delim.py
+++ python-pandas/useful-functions/prefix_delim.py
@@ -1,4 +1,6 @@
-# Method to add a delimiter as prefix
-# Note:- This was added for understanding, however their are pre-built functions in pandas for this task
 def prefix_delim(self, _tgt_col_name, _delim):
+    '''
+    Method to add a delimiter as prefix
+    Note:- This was added for understanding, however their are pre-built functions in pandas for this task
+    '''
     self.my_df[_tgt_col_name] = _delim + self.my_df[_tgt_col_name].astype(str)
