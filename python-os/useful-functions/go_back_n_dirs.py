f73778ce082412a563518849b0fa760d37f57b28
ashishsamarth <108293915+ashishsamarth@users.noreply.github.com>
2025-09-03 20:12:30 -0500

    Update go_back_n_dirs.py
    go_back_n_dirs.py

--- python-os/useful-functions/go_back_n_dirs.py
+++ python-os/useful-functions/go_back_n_dirs.py
@@ -1,9 +1,10 @@
 import os
 
-# Method to go back n number of directories
-# Argument to this method is: number of directories to go back
-
 def go_back_n_dirs(self, num_of_dirs: int):
+    '''
+    Method to go back n number of directories
+    Argument to this method is: number of directories to go back
+    '''
     # method variable to hold the message
     note = 'Input Argument must be valid integer'
     try:
@@ -19,4 +20,5 @@ def go_back_n_dirs(self, num_of_dirs: int):
         # return type of this method is string
         return f'Navigated Back to {self.current_directory}'
     except AssertionError:
+
         return note    
